8f6c5da689cba5c86dca0e8dbe84a5a2e5aac030
usr110 <m.aliabbas@gmail.com>
2018-02-28 14:59:12 +0000

    Fix calculcations for mmet and also read the total dataset - instead of 10k.

--- PA/code/script.R
+++ PA/code/script.R
@@ -2,18 +2,27 @@
 library(tidyverse)
 library(sqldf)
 # Read combined individual travel survey and Physical Activity data
-baseline <- read.csv("PA/data/180219_Metahit10000_v2_nolabel.csv", header = T, stringsAsFactors = F)
+# baseline <- read.csv("PA/data/180219_Metahit10000_v2_nolabel.csv", header = T, stringsAsFactors = F)
+
+baseline <- readstata13::read.dta13("PA/data/SPtrip_CensusNTSAPS_E06000001.dta", convert.factors = T, generate.factors = F)
+
+baseline$trip_cycletime_hr <- baseline$trip_cycletime_min / 60
+
+baseline$trip_walktime_hr <- baseline$trip_walktime_min / 60
 
 # Initialize  energy expenditure constants - taken from ICT
 METCycling <- 5.63
 METWalking <- 3.53
 METEbikes <- 4.50
 
-#calculate mmets in baseline
-# Add minutes from both APS (walking, cycling and total sports) and NTS trips (walking and cycling)
-baseline$METh <- (METWalking * baseline$trip_walktime_min / 60) + (METWalking * baseline$walkrec10_wkhr) +
-  (METCycling * baseline$trip_cycletime_min / 60) + (METWalking * baseline$cyclerec_wkhr / 60) + baseline$sport_wkmets
-baseline$MMETh <- ((METWalking - 1) * baseline$trip_walktime_min / 60) + ((METWalking - 1) * baseline$walkrec10_wkhr) +
-((METCycling - 1) * baseline$trip_cycletime_min / 60) + ((METCycling - 1) * baseline$cyclerec_wkhr) + baseline$sport_wkmets
+individual_mmet <- sqldf('select census_id, female, agecat, sum(trip_cycletime_hr) as cycleNTS_wkhr,
+                         sum(trip_walktime_hr) as walkNTS_wkhr, sport_wkmmets
+                         from baseline group by census_id')
 
-individual_mmet <- sqldf('select f.census_id, sum(f.MMETh) as mmets FROM baseline as f GROUP BY f.census_id')
+individual_mmet$total_mmet <- ((METCycling - 1) * individual_mmet$cycleNTS_wkhr) + 
+                          ((METWalking - 1) * individual_mmet$walkNTS_wkhr) + individual_mmet$sport_wkmmets
+
+individual_mmet$total_mmet <- ifelse(is.na(individual_mmet$total_mmet), 0, individual_mmet$total_mmet)
+
+individual_mmet  %>% group_by(female, agecat) %>% summarise(mean = mean(total_mmet))
+
