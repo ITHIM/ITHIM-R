e9a58d4cb7c98b5549416cd8701ccf63d33807c2
usr110 <m.aliabbas@gmail.com>
2018-02-28 11:49:13 +0000

    Calculate individual total travel and sports mmeth

--- PA/code/script.R
+++ PA/code/script.R
@@ -1,11 +1,19 @@
 # Load packages
 library(tidyverse)
+library(sqldf)
 # Read combined individual travel survey and Physical Activity data
-pop <- read.csv("PA/data/180219_Metahit10000_v2_nolabel.csv", header = T, stringsAsFactors = F)
-# Initialize  energy expenditure constants
-METcycling <- 5.63
-METwalking <- 3.53
-METebikes <- 4.50
+baseline <- read.csv("PA/data/180219_Metahit10000_v2_nolabel.csv", header = T, stringsAsFactors = F)
 
+# Initialize  energy expenditure constants - taken from ICT
+METCycling <- 5.63
+METWalking <- 3.53
+METEbikes <- 4.50
 
-#
+#calculate mmets in baseline
+# Add minutes from both APS (walking, cycling and total sports) and NTS trips (walking and cycling)
+baseline$METh <- (METWalking * baseline$trip_walktime_min / 60) + (METWalking * baseline$walkrec10_wkhr / 60) +
+  (METCycling * baseline$trip_cycletime_min / 60) + (METWalking * baseline$cyclerec_wkhr / 60) + baseline$sport_wkmets
+baseline$MMETh <- ((METWalking - 1) * baseline$trip_walktime_min / 60) + ((METWalking - 1) * baseline$walkrec10_wkhr / 60) +
+((METCycling - 1) * baseline$trip_cycletime_min / 60) + ((METCycling - 1) * baseline$cyclerec_wkhr / 60) + baseline$sport_wkmets
+
+individual_mmet <- sqldf('select f.census_id, sum(f.MMETh) as mmets FROM baseline as f GROUP BY f.census_id')
