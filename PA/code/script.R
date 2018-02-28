b60939c36f8a33264c313c85eb01c6a882f4cf7f
usr110 <m.aliabbas@gmail.com>
2018-02-28 12:08:56 +0000

    Correct formula

--- PA/code/script.R
+++ PA/code/script.R
@@ -11,9 +11,9 @@ METEbikes <- 4.50
 
 #calculate mmets in baseline
 # Add minutes from both APS (walking, cycling and total sports) and NTS trips (walking and cycling)
-baseline$METh <- (METWalking * baseline$trip_walktime_min / 60) + (METWalking * baseline$walkrec10_wkhr / 60) +
+baseline$METh <- (METWalking * baseline$trip_walktime_min / 60) + (METWalking * baseline$walkrec10_wkhr) +
   (METCycling * baseline$trip_cycletime_min / 60) + (METWalking * baseline$cyclerec_wkhr / 60) + baseline$sport_wkmets
-baseline$MMETh <- ((METWalking - 1) * baseline$trip_walktime_min / 60) + ((METWalking - 1) * baseline$walkrec10_wkhr / 60) +
-((METCycling - 1) * baseline$trip_cycletime_min / 60) + ((METCycling - 1) * baseline$cyclerec_wkhr / 60) + baseline$sport_wkmets
+baseline$MMETh <- ((METWalking - 1) * baseline$trip_walktime_min / 60) + ((METWalking - 1) * baseline$walkrec10_wkhr) +
+((METCycling - 1) * baseline$trip_cycletime_min / 60) + ((METCycling - 1) * baseline$cyclerec_wkhr) + baseline$sport_wkmets
 
 individual_mmet <- sqldf('select f.census_id, sum(f.MMETh) as mmets FROM baseline as f GROUP BY f.census_id')
