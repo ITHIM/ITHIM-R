bb3f3cfff4499f260b6a00a0e9f19ebbedae38e3
usr110 <m.aliabbas@gmail.com>
2018-03-08 12:41:53 +0000

    Set column name and class for create.lookups

--- PA/code/functions.R
+++ PA/code/functions.R
@@ -14,12 +14,15 @@ clear.labels <- function(x) {
 
 
 create.lookups <- function(df, cols){
-  lt <- data.frame(name = NULL, id = NULL, val = NULL)
+  lt <- data.frame("name" = as.character(), "id" = as.integer(), "val" = as.character())
+  # df <- raw_data
+  # cols <- c("female", "agecat", "agecat_det", "trip_mainmode")
   for (i in 1:length(cols)){
     val <- as.character(unique(as_factor(df[[cols[i]]], labels = "values")))
     id <- unique(df[[cols[i]]])
     m <- matrix(c(rep(cols[i], length(id)), id, val), nrow = length(id), ncol = 3)
-    lt <- rbind(lt, as.data.frame(m))
+    lt <- rbind(lt, as.data.frame(m, stringsAsFactors = F))
   }
+  colnames(lt) <- c("names", "id", "val")
   lt
 }
