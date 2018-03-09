fd4a4aa3967306750e875a7594cc4586892cbac8
Rob Johnson <rob.johnson@mrc-bsu.cam.ac.uk>
2018-03-09 18:16:13 +0000

    more functional, with linear calling script

---
+++ injuries/linear_injury.R
@@ -0,0 +1,56 @@
+setwd('/home/rob/overflow_dropbox/ITHIM-R/injuries/')
+source('shinyinjury.R')
+
+input <- list()
+input$injuryfile <- 'input_injuries.xlsx'
+input$distancefile <- 'synthetic_population.ods'
+input$file2 <- 'save_mexico.Rdata'
+input$group <- 'Strike mode'
+input$over <- 'Casualty age'
+input$subgroup <- 'cyclist'
+input$SE <- T
+input$lq <- 0.25
+input$uq <- 0.75
+inFile <- list()
+object_store <- list(fit=NULL,tab=NULL,injuries=NULL,plotButton=NULL,covariates=NULL,
+  mexicoButton=NULL,distance=NULL,model='poisson')
+
+
+model <- 'poisson'
+if (model=='NB') object_store$model <- 'NB'
+
+useShiny <- F
+useMexicoModel <- T
+
+if(useMexicoModel==F){
+  ## GET INJURY INPUT
+  inFile$datapath <- input$injuryfile
+  if (is.null(inFile)) return(NULL)
+  ##TODO allow many file types
+  injuries <- read.xlsx(inFile$datapath,sheetIndex=1,stringsAsFactors=FALSE,header=F)
+  ##TODO many checks here
+  # are the ages interpretable? How are they coded?
+  # are the ages the same as the distance ages?
+  # are there travel data for hgvs?
+  # what are the covariates? are they all in the distance data?
+  # V codes comprehensible, numbers as numbers not strings...
+  object_store$injuries <- injuries
+  
+  inFile$datapath <- input$distancefile
+  if (is.null(inFile)) return(NULL)
+  distance <- read.ods(inFile$datapath)[[1]]
+  object_store$distance <- distance
+  
+  object_store <- fitModel(object_store)
+  
+}else{
+  inFile$datapath <- input$file2
+  if (is.null(inFile)) return(NULL)
+  object_store_temp <- readRDS(inFile$datapath)
+  for(x in names(object_store_temp)) object_store[[x]] <- object_store_temp[[x]]
+}
+
+prepPlots(object_store,input)
+
+
+
