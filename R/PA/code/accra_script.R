56d936e6c90a1eeed75e43f9cbf2f28e1f192ac4
usr110 <m.aliabbas@gmail.com>
2018-06-27 16:16:13 +0100

    Update class of columns

--- R/PA/code/accra_script.R
+++ R/PA/code/accra_script.R
@@ -8,3 +8,7 @@ library(plotly)
 # Read travel survey data
 raw_data <- readRDS("data/synth_pop_data/accra/travel_survey/accra_processed_trips.Rds")
 
+raw_data[,c("trip_id", "participant_id", "age")] <- lapply(raw_data[,c("trip_id", "participant_id", "age")], as.numeric)
+raw_data[,c("trip_duration", "trip_distance")] <- lapply(raw_data[,c("trip_duration", "trip_distance")], as.double)
+
+
