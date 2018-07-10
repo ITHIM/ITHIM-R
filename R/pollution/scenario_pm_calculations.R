0b1286bafc2aa12ac84dadc1a9cca0dc0117f351
rahulatiitd <rahulatiitd@gmail.com>
2018-07-10 09:45:28 +0100

    added transport emision inventory/ added pm concentration calculation code

---
+++ R/pollution/scenario_pm_calculations.R
@@ -0,0 +1,3 @@
+### this code is to calculate daily PM concentrations for the baseline and the scenarios
+ind <- read_csv("data/synth_pop_data/accra/processed_data/scenarios/baseline_and_three_scenarios.csv")
+ind<-as.data.frame(ind)
