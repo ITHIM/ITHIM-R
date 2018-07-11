e288595d0ff453aa4bf84bcb404f41fd99bde3e4
usr110 <m.aliabbas@gmail.com>
2018-07-11 17:06:37 +0100

    Calculate total mmet per person per week for baseline and scenarios

--- R/PA/accra/total_mmet.R
+++ R/PA/accra/total_mmet.R
@@ -30,22 +30,22 @@ MMETEbikes <- 4.50 - 1
 # Convert baseline's trip duration from mins to hours
 rd$trip_duration_hrs <- rd$trip_duration / 60
 
-
 # Get total individual level walking and cycling and sport mmets 
 ind <- rd %>% group_by(participant_id) %>% summarise ( sex = first(sex), 
+                                                       age = first(age),
                                                        age_cat = first(age_cat), 
-                                                       cycling_mmet_base = (sum(trip_duration_hrs[trip_mode == 'Bicycle']) * MMETCycling),
-                                                       walking_mmet_base = ((sum(trip_duration_hrs[trip_mode == 'Walking']) + 
-                                                                               sum(trip_duration_hrs[trip_mode == 'Short Walking'])) * MMETCycling),
-                                                       cycling_mmet_scen1 = (sum(trip_duration_hrs[scen1_mode == 'Bicycle']) * MMETCycling),
-                                                       walking_mmet_scen1 = ((sum(trip_duration_hrs[scen1_mode == 'Walking']) + 
-                                                                                sum(trip_duration_hrs[scen1_mode == 'Short Walking'])) * MMETCycling),
-                                                       cycling_mmet_scen2 = (sum(trip_duration_hrs[scen2_mode == 'Bicycle']) * MMETCycling),
-                                                       walking_mmet_scen2 = ((sum(trip_duration_hrs[scen2_mode == 'Walking']) + 
-                                                                                sum(trip_duration_hrs[scen2_mode == 'Short Walking'])) * MMETCycling),
-                                                       cycling_mmet_scen3 = (sum(trip_duration_hrs[scen3_mode == 'Bicycle']) * MMETCycling),
-                                                       walking_mmet_scen3 = ((sum(trip_duration_hrs[scen3_mode == 'Walking']) + 
-                                                                                sum(trip_duration_hrs[scen3_mode == 'Short Walking'])) * MMETCycling),
+                                                       cycling_mmet_base = (sum(trip_duration_hrs[trip_mode == 'Bicycle' & scenario == 'Baseline']) * MMETCycling),
+                                                       walking_mmet_base = ((sum(trip_duration_hrs[trip_mode == 'Walking'  & scenario == 'Baseline']) + 
+                                                                               sum(trip_duration_hrs[trip_mode == 'Short Walking'  & scenario == 'Baseline'])) * MMETCycling),
+                                                       cycling_mmet_scen1 = (sum(trip_duration_hrs[trip_mode == 'Bicycle'  & scenario == 'Scenario 1']) * MMETCycling),
+                                                       walking_mmet_scen1 = ((sum(trip_duration_hrs[trip_mode == 'Walking'  & scenario == 'Scenario 1']) + 
+                                                                                sum(trip_duration_hrs[trip_mode == 'Short Walking'   & scenario == 'Scenario 1'])) * MMETCycling),
+                                                       cycling_mmet_scen2 = (sum(trip_duration_hrs[trip_mode == 'Bicycle'   & scenario == 'Scenario 2']) * MMETCycling),
+                                                       walking_mmet_scen2 = ((sum(trip_duration_hrs[trip_mode == 'Walking'   & scenario == 'Scenario 2']) + 
+                                                                                sum(trip_duration_hrs[trip_mode == 'Short Walking'   & scenario == 'Scenario 2'])) * MMETCycling),
+                                                       cycling_mmet_scen3 = (sum(trip_duration_hrs[trip_mode == 'Bicycle'   & scenario == 'Scenario 3']) * MMETCycling),
+                                                       walking_mmet_scen3 = ((sum(trip_duration_hrs[trip_mode == 'Walking'   & scenario == 'Scenario 3']) + 
+                                                                                sum(trip_duration_hrs[trip_mode == 'Short Walking'  & scenario == 'Scenario 3'])) * MMETCycling),
                                                        work_ltpa_mmet = first(work_ltpa_marg_met),
                                                        base_mmet = first(work_ltpa_marg_met) +  cycling_mmet_base + walking_mmet_base,
                                                        scen1_mmet = first(work_ltpa_marg_met) +  cycling_mmet_scen1 + walking_mmet_scen1,
@@ -54,6 +54,7 @@ ind <- rd %>% group_by(participant_id) %>% summarise ( sex = first(sex),
                                                        
 )
 
-write.csv(ind,'~/GitHub/ITHIM-R/data/synth_pop_data/accra/processed_data/indiv_mmet/pa_total_mmet_weekly.csv')
 
-str(ind)
+ind <- select(ind, participant_id, sex, age, age_cat, base_mmet, scen1_mmet, scen2_mmet, scen3_mmet)
+
+write.csv(ind,'data/synth_pop_data/accra/pa/pa_total_mmet_weekly.csv')
