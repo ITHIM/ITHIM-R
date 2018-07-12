da4d5839b58df8c1600fe9f72f7e4d6f5323f850
usr110 <m.aliabbas@gmail.com>
2018-07-12 12:37:00 +0100

    Factorize

--- R/health/accra/health_burden.R
+++ R/health/accra/health_burden.R
@@ -1,47 +1,18 @@
 source("R/scenarios/accra/setup.R")
 
-# Read the ind file with rr for pa and ap
-ind <- read_csv("data/synth_pop_data/accra/RR/RR_AP_calculations.csv")
+# Example code for health burden calculations for RR_pa_ap_base_lc and RR_pa_ap_base_lc
 
-# Replace NaNs with 1
-is.nan.data.frame <- function(x)
-  do.call(cbind, lapply(x, is.nan))
+source("R/PA/code/functions.R")
 
-ind[is.nan(ind)] <- 1
+ind <- read_csv("data/synth_pop_data/accra/RR/RR_PA_AP_calculations.csv")
 
-# Read disease lt
-disease_lt <- read.csv("data/dose_response/disease_outcomes_lookup.csv")
-
-disease_lt[is.na(disease_lt)] <- 0
-
-for (i in 1:nrow(disease_lt)){
-  
-  for (scen in c('base', 'scen1', 'scen2', 'scen3')){
-    # i <- 1
-    if (disease_lt$air_pollution[i] == 1 & disease_lt$physical_activity[i] ){
-      
-      ind[[paste0(scen, '_rr_ap_pa_', disease_lt$acronym[i])]] <- runif(nrow(ind), min = 0, max = 1)
-      
-    }
-    else if (disease_lt$air_pollution[i] == 1 & disease_lt$physical_activity[i] != 1){
-      
-      ind[[paste0(scen, '_rr_ap_', disease_lt$acronym[i])]] <- runif(nrow(ind), min = 0, max = 1)
-      
-    } else if (disease_lt$air_pollution[i] != 1 & disease_lt$physical_activity[i] == 1){
-      
-      ind[[paste0(scen, '_rr_pa_', disease_lt$acronym[i])]] <- runif(nrow(ind), min = 0, max = 1)
-    }
-    
-  }
-}
-
-pif <- data.frame(PAF(pop = ind, attr = c('sex', 'age_cat'), cn = c('base_rr_ap_pa_cvd', 'scen1_rr_ap_pa_cvd')))
+pif <- data.frame(PAF(pop = ind, attr = c('sex', 'age_cat'), cn = c('RR_pa_ap_base_lc', 'RR_pa_ap_scen1_lc')))
 pif <- arrange(pif, age.band, gender)
 
 
 # Redefine non-factor based column classes
 pif[,c("age.band", "gender")] <- lapply(pif[,c("age.band", "gender")], as.character)
-pif[,c("base_rr_ap_pa_cvd", "scen1_rr_ap_pa_cvd")] <- lapply(pif[,c("base_rr_ap_pa_cvd", "scen1_rr_ap_pa_cvd")], as.double)
+pif[,c("RR_pa_ap_base_lc", "RR_pa_ap_scen1_lc")] <- lapply(pif[,c("RR_pa_ap_base_lc", "RR_pa_ap_scen1_lc")], as.double)
 
 
 # Read gbd data
@@ -51,7 +22,7 @@ yll_dfs <- combine_health_and_pif(
   pop = pif,
   hc = gbd_data,
   hm = "YLLs (Years of Life Lost)",
-  cn = c("base_rr_ap_pa_cvd", "scen1_rr_ap_pa_cvd"),
+  cn = c("RR_pa_ap_base_lc", "RR_pa_ap_scen1_lc"),
   hm_cause <- "All causes",
   hm_cn <- 'val_accra')
 
@@ -66,7 +37,7 @@ death_dfs <- combine_health_and_pif(
   pop = pif,
   hc = gbd_data,
   hm = "Deaths",
-  cn = c("base_rr_ap_pa_cvd", "scen1_rr_ap_pa_cvd"),
+  cn = c("RR_pa_ap_base_lc", "RR_pa_ap_scen1_lc"),
   hm_cause <- "All causes",
   hm_cn <- 'val_accra')
 
