114796114ff574e2ca894f5cd2a4c8d396980447
usr110 <m.aliabbas@gmail.com>
2018-07-13 11:04:15 +0100

    Append injuries deaths and ylls to the total deaths and ylls tables

--- R/health/accra/health_burden.R
+++ R/health/accra/health_burden.R
@@ -285,17 +285,25 @@ for ( j in 1:nrow(disease_lt)){
 
 
 # read injuries dataset for both ylls and deaths
-#inj <- read_csv("R/injuries/accra/deaths_yll_injuries.csv")
+inj <- read_csv("R/injuries/accra/deaths_yll_injuries.csv")
 
 # rename columns
-# inj <- rename(inj, age.band = age_cat, gender = sex)
-# 
-# inj_deaths <- select(inj, c(age.band, gender, contains("deaths")))
-# 
-# inj_ylls <- select(inj, c(age.band, gender, contains("yll")))
-# 
-# gdeaths <- left_join(gdeaths, inj_deaths)
-# gylls <- left_join(gylls, inj_yll
+inj <- rename(inj, age.band = age_cat, gender = sex)
+
+inj_deaths <- select(inj, c(age.band, gender, contains("deaths")))
+
+inj_ylls <- select(inj, c(age.band, gender, contains("yll")))
+
+gdeaths <- left_join(gdeaths, inj_deaths)
+gylls <- left_join(gylls, inj_ylls)
+
+write_csv(gdeaths, "data/scenarios/accra/health_burden/total_deaths.csv")
+write_csv(gdeaths_red, "data/scenarios/accra/health_burden/total_deaths_red.csv")
+write_csv(gylls, "data/scenarios/accra/health_burden/total_ylls.csv")
+write_csv(gylls_red, "data/scenarios/accra/health_burden/total_ylls_red.csv")
+
+
+
 
 
 
