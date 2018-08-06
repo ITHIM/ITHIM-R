6519730b04929bc6d96035a471574105cee493d5
usr110 <m.aliabbas@gmail.com>
2018-08-06 17:13:35 +0100

    Remove drpa

--- R/scenarios/accra/setup.R
+++ R/scenarios/accra/setup.R
@@ -1,6 +1,7 @@
 # Clear workspace
 rm (list = ls())
-
+source("R/drpa/dose_response.R")
 # Load base required libraries
-pkgs <- c("tidyverse", "drpa")
+pkgs <- c("tidyverse")
+
 lapply(pkgs, library, character.only = T)
