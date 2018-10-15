f227e20d74283b9c666da6dc2b28465dd3ce24b9
usr110 <m.aliabbas@gmail.com>
2018-10-15 16:02:07 +0100

    Calculate pif_interpol for all diseases

--- code/MSLT/pif_calculations.R
+++ code/MSLT/pif_calculations.R
@@ -1,13 +1,15 @@
 rm (list = ls())
 
+require(dplyr)
+require(tidyverse)
+
 # load dr for pa objects
 source("code/drpa/dose_response.R")
 
 # Load all health related functions
 source("code/PA/code/functions.R")
 
-require(dplyr)
-require(tidyverse)
+
 
 mmets <- read_csv("code/MSLT/data/england/mmets_london.csv")
 
@@ -80,7 +82,7 @@ for ( j in 1:nrow(disease_short_names)){
                                                                             use_75_pert = F)$rr %>% as.numeric()
       }else if(local_cause == 'diabetes'){
         mmets[[paste('RR_pa', scen, pa_dn, sep = '_')]][i] <- dose_response(cause = local_cause, outcome_type = 'mortality', certainty = pa_certainty, 
-                                                                         dose = mmets[[scen]][i], use_75_pert = F)$rr %>% as.numeric()
+                                                                            dose = mmets[[scen]][i], use_75_pert = F)$rr %>% as.numeric()
         
       }else if(local_cause == 'total-cancer'){
         mmets[[paste('RR_pa', scen, pa_dn, sep = '_')]][i] <- dose_response(cause = local_cause, outcome_type = 'mortality', certainty = pa_certainty, 
@@ -93,10 +95,90 @@ for ( j in 1:nrow(disease_short_names)){
   
 }
 
-mmets[is.na(mmets$RR_pa_baseline_ac),]$RR_pa_baseline_ac <- 0
-mmets[is.na(mmets$RR_pa_MS0.05_ebik0_eq0_ac),]$RR_pa_MS0.05_ebik0_eq0_ac <- 0
+pif_list <- list()
+index <- 1
 
-# Calculate PIFs for baseline and selected scenario
-pif <- data.frame(PAF(pop = mmets, attr = c('Sex_B01ID', 'age_group'), cn = c("RR_pa_baseline_ac" , "RR_pa_MS0.05_ebik0_eq0_ac")))
-pif <- arrange(pif, age.band, gender)
+for ( j in 1:nrow(disease_short_names)){
+  ## checking whether to calculate this health outcome for PA
+  pa_dn <- disease_short_names$sname[j] %>% as.character()
+  local_cause <- disease_short_names$disease[j] %>% as.character()
+  
+  for (scen in names(mmets %>% dplyr::select(starts_with("MS")))){
+    
+    qb <- paste('RR_pa_baseline', pa_dn, sep = '_')
+    
+    qscen <- paste('RR_pa', scen, pa_dn, sep = '_')
+    
+    # browser()
+    
+    if (nrow(mmets[is.na(mmets[[qb]]),]) > 0)
+      mmets[is.na(mmets[[qb]]),][[qb]] <- 0
+    if (nrow(mmets[is.na(mmets[[qscen]]),]) > 0) 
+      mmets[is.na(mmets[[qscen]]),][[qscen]]
+    
+    # Calculate PIFs for baseline and selected scenario
+    pif <- data.frame(PAF(pop = mmets, attr = c('Sex_B01ID', 'age_group'), cn = c(qb , qscen)))
+    pif <- arrange(pif, age.band, gender)
+    
+    pif[, 3] <- NULL
+    
+    pif$gender <- as.integer(pif$gender)
+    pif$age.band <- (as.character(pif$age.band))
+    pif[[qscen]] <- as.numeric(as.character(pif[[qscen]]))
+    
+    pif_interp <- data.frame(age = rep(seq(0, 100), 2), sex = c(rep("male", 101), rep("female", 101)))
+    
+    pif_interp$sex <- as.character(pif_interp$sex)
+    
+    for (i in 1:nrow(pif)){
+      
+      pif$age_group[i] <- round(mean(as.numeric(str_extract_all(pif$age.band[i],"\\(?[0-9,.]+\\)?")[[1]])))
+      
+    }
+    
+    pif_interp$v <- -1
+    
+    for (i in 1:(nrow(pif) / 2)){
+      for (j in unique(pif$gender)){
+        
+        local_pif <- filter(pif, gender == j)
+        
+        sex <- ifelse (j == 1, "male", "female")
+        val <- local_pif[i, 3]
+        
+        if (i == 1){
+          pif_interp[pif_interp$sex == sex &
+                       pif_interp$age <= local_pif$age_group[i],]$v <- val
+        }
+        
+        if (i > 1 && i != (nrow(local_pif))){
+          pif_interp[pif_interp$sex == sex &
+                       (pif_interp$age <= local_pif$age_group[i] &
+                          pif_interp$age > local_pif$age_group[i - 1]),]$v <- val
+          
+        }
+        
+        if (i == nrow(local_pif)){
+          pif_interp[pif_interp$sex == sex &
+                       pif_interp$age > local_pif$age_group[i],]$v <- val
+        }
+        
+      }
+      
+    }
+    
+    
+    # browser()
+    pif_list[[index]] <- pif_interp
+    index <- index + 1
+    
+    
+    
+  }
+}
+
+# mmets[is.na(mmets$RR_pa_baseline_ac),]$RR_pa_baseline_ac <- 0
+# mmets[is.na(mmets$RR_pa_MS0.05_ebik0_eq0_ac),]$RR_pa_MS0.05_ebik0_eq0_ac <- 0
+
+
 
