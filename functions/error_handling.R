790e17d0505d2375631e9f9c7c37f488b861ea3b
Javad Rahimipour Anaraki <35808171+jracp@users.noreply.github.com>
2018-12-06 14:34:03 -0330

    A new function to handle errors and warnings

---
+++ functions/error_handling.R
@@ -0,0 +1,17 @@
+
+error_handling <- function(code, function_name, variable_name) {
+  output <-
+    switch(
+      code,
+      paste0(
+        "In ",
+        function_name,
+        "(",
+        variable_name,
+        "): Default values should be changed!"
+      )
+    )
+  
+  print(output)
+  
+}
