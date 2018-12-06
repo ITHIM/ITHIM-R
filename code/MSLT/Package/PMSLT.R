183398fdc8d691c44eae468a5165c0450be41d22
usr110 <m.aliabbas@gmail.com>
2018-12-06 10:28:43 +0000

    Remove MSLT folder

--- code/MSLT/Package/PMSLT.R
+++
@@ -1,8 +0,0 @@
-###Packages
-
-require(c("devtools", "roxygen2", "testthat", "knitr"))
-
-###Use the following code to access new devtools functions as I develop them. This is particularly important during the development of the book.
-
-
-devtools::install_github("hadley/devtools")
