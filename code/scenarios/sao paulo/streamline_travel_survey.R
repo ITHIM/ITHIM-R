564159d9a65ab5321bf3a1f0db45f32703dbc476
usr110 <m.aliabbas@gmail.com>
2019-01-31 16:49:19 +0000

    Add secular trends and printing

--- code/scenarios/sao paulo/streamline_travel_survey.R
+++ code/scenarios/sao paulo/streamline_travel_survey.R
@@ -313,3 +313,47 @@ ithim_object$outcomes <- run_ithim(ithim_object, seed = 1)
     }
   }
 }
+
+ithim_object$outcome <- list()
+
+## what if: cleaner fleet !! not yet implemented
+ithim_object$parameters <- ithim_setup_parameters()
+ithim_object$outcome$cleaner_fleet <- run_ithim(ithim_object, seed = 1)
+
+## what if: the roads are safer
+ithim_object$parameters <- ithim_setup_parameters(INJURY_REPORTING_RATE = 2)
+ithim_object$outcome$safety <- run_ithim(ithim_object, seed = 1)
+
+## what if: the rate of chronic disease doubles
+ithim_object$parameters <- ithim_setup_parameters(CHRONIC_DISEASE_SCALAR = 2)
+ithim_object$outcome$chronic_disease <- run_ithim(ithim_object, seed = 1)
+
+## what if: non-transport air pollution is half
+ithim_object$parameters <- ithim_setup_parameters(PM_CONC_BASE = 30.625, PM_TRANS_SHARE = 0.3673469)
+ithim_object$outcome$background_ap <- run_ithim(ithim_object, seed = 1)
+
+## what if: non-transport physical activity is half
+ithim_object$parameters <- ithim_setup_parameters(BACKGROUND_PA_SCALAR = 0.5)
+ithim_object$outcome$background_pa <- run_ithim(ithim_object, seed = 1)
+
+
+## plot results
+result_list <- lapply(ithim_object$outcome,function(x)colSums(x$hb$ylls[,3:ncol(x$hb$ylls)]))
+result_mat <- do.call(rbind, result_list)
+columns <- ncol(result_mat)
+nDiseases <- columns/NSCEN
+for(i in 1:nDiseases){
+  x11(width = 8, height = 5); par(mfrow = c(2, 3))
+  ylim <- range(result_mat[, 1:NSCEN+(i-1)*NSCEN])
+  for(j in 1:nrow(result_mat)){
+    if(j<4) {
+      par(mar = c(1, 4, 4, 1))
+      barplot(result_mat[j, 1:NSCEN + (i - 1) * NSCEN], names.arg = '', ylim = ylim, las = 2, 
+              main = paste0(rownames(result_mat)[j], ', ', last(strsplit(colnames(result_mat)[i * NSCEN], '_')[[1]])))
+    }else{
+      par(mar = c(5, 4, 4, 1))
+      barplot(result_mat[j, 1:NSCEN + (i - 1) * NSCEN], names.arg = SCEN_SHORT_NAME[c(1, 3:6)], ylim = ylim, las = 2, 
+              main = paste0(rownames(result_mat)[j], ',  ', last(strsplit(colnames(result_mat)[i * NSCEN], '_')[[1]])))
+    }
+  }
+}
