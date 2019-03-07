efcb0071ca9158d191335eee1e82a44c72e7d2f4
usr110 <m.aliabbas@gmail.com>
2019-03-07 15:42:50 +0000

    Use accra test template for ithim_setup function

--- delhi_master_script.R
+++ delhi_master_script.R
@@ -6,14 +6,12 @@ rm (list = ls())
   #require(tidyverse)
   
   ithim_object <- run_ithim_setup(CITY = 'delhi',
+                                  TEST_WALK_SCENARIO = T,
                                   ADD_WALK_TO_BUS_TRIPS = F,
-                                  ADD_BUS_DRIVERS = F,
                                   ADD_TRUCK_DRIVERS = F,
-                                  TEST_WALK_SCENARIO = F,
-                                  BUS_WALK_TIME= 5,
-                                  PM_TRANS_SHARE = 0.225
-  )
+                                  ADD_BUS_DRIVERS = F)
   
+
   ithim_object$outcomes <- run_ithim(ithim_object, seed = 1)
   
 }
