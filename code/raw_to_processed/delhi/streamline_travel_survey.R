ce81e46a21928c212edc0836d29b476e105cfd24
usr110 <m.aliabbas@gmail.com>
2019-03-12 15:34:50 +0000

    Rename columns

--- code/raw_to_processed/delhi/streamline_travel_survey.R
+++ code/raw_to_processed/delhi/streamline_travel_survey.R
@@ -145,5 +145,11 @@ rd$hh_id <- NULL
 rd$total_distance <- ave(rd$distance, rd$trip_id, FUN=sum)
 
 #####
+## Rename columns
+
+rd1 <- rd %>% rename(stage_mode_int = mode, stage_mode = mode_name, stage_distance = distance, stage_duration = duration, trip_mode_int = main_mode,
+                     trip_mode = main_mode_name, trip_distance = total_distance)
+
+#####
 # Write streamlined travel survey data as a csv in the inst folder
 write_csv(rd, "inst/extdata/local/delhi/delhi_travel_survey.csv")
