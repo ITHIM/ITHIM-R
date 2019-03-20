b6c24b15caaa7146155d65069527cd733faae8c5
usr110 <m.aliabbas@gmail.com>
2019-03-20 14:33:21 +0000

    Rename var name and indent

--- delhi_master_script.R
+++ delhi_master_script.R
@@ -5,13 +5,13 @@ rm (list = ls())
 require(ithimr)
 #require(tidyverse)
 
-ithim_object <- run_ithim_setup(CITY = 'delhi',
+io <- run_ithim_setup(CITY = 'delhi',
                                 TEST_WALK_SCENARIO = T,
                                 ADD_WALK_TO_BUS_TRIPS = F,
                                 ADD_TRUCK_DRIVERS = F,
                                 ADD_BUS_DRIVERS = F,
-                                speeds = list(subway=32,
-                                              bicycle=15),
+                                speeds = list(subway = 32,
+                                              bicycle = 15),
                                 emission_inventory = list(motorcycle = 1409,
                                                           auto_rickshaw = 133,
                                                           car = 2214,
@@ -22,4 +22,4 @@ ithim_object <- run_ithim_setup(CITY = 'delhi',
                                                           other = 0,
                                                           taxi = 0))
 
-ithim_object$outcomes <- run_ithim(ithim_object, seed = 1)
+io$outcomes <- run_ithim(io, seed = 1)
