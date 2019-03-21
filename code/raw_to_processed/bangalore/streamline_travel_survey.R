0dce1237a6911d02deeac2eab5e1b41ab8d1ad22
usr110 <m.aliabbas@gmail.com>
2019-03-21 15:10:34 +0000

    Remove duplicates

--- code/raw_to_processed/bangalore/streamline_travel_survey.R
+++ code/raw_to_processed/bangalore/streamline_travel_survey.R
@@ -7,3 +7,26 @@ library(plotly)
 
 # Read bangalore travel survey with stages
 raw_data <- read.csv("data/local/bangalore/bangalore_travel_survey_2011_March_14.csv", stringsAsFactors = F)
+
+#####
+## Remove duplicate rows
+raw_data <- raw_data[!duplicated(raw_data), ]
+
+## Remove duplicated rows
+raw_data <- raw_data[(!duplicated(raw_data$uniqueid )) | (raw_data$uniqueid == "NA_NA"),]
+
+## Remove uniqueid
+raw_data$uniqueid <- NULL
+
+# Save read data in a separate var
+rd <- raw_data
+
+#####
+## Remove duplicate rows for people without trips
+rd_pwt <- rd %>% filter(!is.na(trip_id))
+
+rd_pwot <- rd %>% filter(is.na(trip_id))
+
+rd_pwot <- rd_pwot[!duplicated(rd_pwot$person_id), ]
+
+rd <- rbind(rd_pwt, rd_pwot)
