927595dbe46c6d9c0ac320a0c58c1220733a8521
Rob Johnson <rob.johnson@mrc-bsu.cam.ac.uk>
2019-04-04 16:26:37 +0100

    graph labels

--- R/summarise_ithim_inputs.R
+++ R/summarise_ithim_inputs.R
@@ -4,11 +4,11 @@ summarise_ithim_inputs <- function(ithim_object){
   
   distances <- as.matrix(ithim_object$dist[,-1])
   distances_pc <- apply(distances,2,function(x)x/sum(x))
-  x11(); par(mar=c(5,5,2,8)); barplot(distances_pc,col=rainbow(length(modenames)),legend.text=modenames,args.legend = c(x=length(SCEN)+4),ylab='Distance share',main=CITY)
+  x11(); par(mar=c(5,5,2,8)); barplot(distances_pc,col=rainbow(length(modenames)),legend.text=modenames,args.legend = c(x=length(SCEN)+4),ylab='Mode share by distance',main=CITY)
   
   trips <- sapply(SCEN,function(y)sapply(modenames,function(x)nrow(subset(subset(ithim_object$trip_scen_sets,trip_mode==x&scenario==y),!duplicated(trip_id)))))
   trips <- apply(trips,2,function(x)x/sum(x))
-  x11(); par(mar=c(5,5,2,8)); barplot(trips,col=rainbow(length(modenames)),legend.text=modenames,args.legend = c(x=length(SCEN)+4),ylab='Mode share',main=CITY)
+  x11(); par(mar=c(5,5,2,8)); barplot(trips,col=rainbow(length(modenames)),legend.text=modenames,args.legend = c(x=length(SCEN)+4),ylab='Mode share by trip mode',main=CITY)
   
   injuries <- sapply(unique(INJURY_TABLE$whw$cas_mode),function(x)sum(subset(INJURY_TABLE$whw,cas_mode==x)$count)+sum(subset(INJURY_TABLE$noov,cas_mode==x)$count))
   names(injuries) <- unique(INJURY_TABLE$whw$cas_mode)
@@ -21,6 +21,6 @@ summarise_ithim_inputs <- function(ithim_object){
   
   emissions <- unlist(EMISSION_INVENTORY)
   emissions <- emissions[emissions>0]
-  x11(); pie(emissions,main=CITY)
+  x11(); pie(emissions,main=paste0(CITY,' emissions'))
 
 }
