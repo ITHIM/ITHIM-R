ed80f277ccd5b741d6177cbbaf77a89c1819e62e
usr110 <m.aliabbas@gmail.com>
2019-04-08 15:05:15 +0100

    Add a save command to write io object

--- multi_city_script.R
+++ multi_city_script.R
@@ -165,6 +165,11 @@ for(city in cities){
   }
 }
 
+## Save the ithim_object in the results folder
+##########
+
+saveRDS(ithim_objects, "C:/RStudio Projects/ITHIM-R/results/multi_city/io.rds")
+
 #################################################
 ## with uncertainty
 ## comparison across cities
