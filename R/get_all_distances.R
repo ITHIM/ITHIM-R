e972e2339935a610ced57b8cb6530a228f12be4c
Rob Johnson <rob.johnson@mrc-bsu.cam.ac.uk>
2019-04-09 14:08:32 +0100

    add walk to subway, rail, minibus and bus trips

--- R/get_all_distances.R
+++ R/get_all_distances.R
@@ -2,7 +2,7 @@
 get_all_distances <- function(ithim_object){
   
   # add walk-to-bus trips, as appropriate, and combines list of scenarios
-  ithim_object$trip_scen_sets <- walk_to_bus_and_combine_scen()
+  ithim_object$trip_scen_sets <- walk_to_pt_and_combine_scen()
   
   # Generate distance and duration matrices
   dist_and_dir <- dist_dur_tbls(ithim_object$trip_scen_sets)
