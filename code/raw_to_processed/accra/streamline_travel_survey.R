0802bd64f5efe831c3888da9e7c7ec5086b07b9f
usr110 <m.aliabbas@gmail.com>
2019-04-30 11:05:42 +0100

    Add 14 motorcycle trips

--- code/raw_to_processed/accra/streamline_travel_survey.R
+++ code/raw_to_processed/accra/streamline_travel_survey.R
@@ -79,3 +79,42 @@ if('stage_duration'%in%colnames(raw_trip_set)&&!'stage_distance'%in%colnames(raw
 if(!'trip_distance'%in%colnames(raw_trip_set))
   raw_trip_set$trip_distance <- sapply(raw_trip_set$trip_id,function(x)sum(subset(raw_trip_set,trip_id==x)$stage_distance))
 
+
+# Define constants
+DISTANCE_SCALAR_CAR_TAXI <- 1
+MOTORCYCLE_TO_CAR_RATIO <- 0.2
+
+total_car_distance <- sum(subset(raw_trip_set,stage_mode=='car')$trip_distance)*DISTANCE_SCALAR_CAR_TAXI
+
+# Redefine motorcycle mode for a select 14 rows
+raw_trip_set$trip_mode[raw_trip_set$trip_mode=='other'][1:14] <- 'motorcycle'
+raw_trip_set <- subset(raw_trip_set,trip_mode!='other')
+raw_trip_set$stage_mode[raw_trip_set$stage_mode=='other'][1:14] <- 'motorcycle'
+raw_trip_set <- subset(raw_trip_set,stage_mode!='other')
+
+# Create new motorbike trips
+# Add 4 new people with 3 trips each
+# Age: 15-59 and gender: male
+new_mode <- 'motorcycle'
+total_mc_distance <- total_car_distance*MOTORCYCLE_TO_CAR_RATIO
+residual_mc_distance <- total_mc_distance - sum(subset(raw_trip_set,stage_mode==new_mode)$trip_distance)
+#duration_range <- 15:100
+nTrips <- 1
+nPeople <- 20
+distance <- residual_mc_distance/nPeople
+new_gender <- c(rep('Male',20),'Female')
+age_range <- 15:69
+speed <- MODE_SPEEDS$speed[MODE_SPEEDS$stage_mode==new_mode]
+
+for(i in 1:nPeople){
+  new_trips <- add_trips(trip_ids   = max(raw_trip_set$trip_id) + 1: nTrips, 
+                         new_mode = new_mode, 
+                         distance = distance, 
+                         participant_id = max(raw_trip_set$participant_id) + 1,
+                         age = age_range,
+                         sex = new_gender,
+                         nTrips=nTrips,
+                         speed=speed)
+  # Add new motorbikes trips to baseline
+  raw_trip_set <- rbind(raw_trip_set, new_trips)
+}
