cd69f5abe80f2e14b43f67e8bd7fd8ce38aa896e
Rob Johnson <rob.johnson@mrc-bsu.cam.ac.uk>
2019-05-05 21:17:15 +0100

    improve speed by removing subset and sapply

--- R/gen_ap_rr.R
+++ R/gen_ap_rr.R
@@ -7,12 +7,9 @@ gen_ap_rr <- function(pm_conc_pp){
   
   ## assigning air pollution age band to the individual_level data
   min_ages <- c(seq(24,94,by=5),200)
-  pm_rr_pp$ap_age <-
-    sapply(pm_rr_pp$age, function(x)
-      if(x > min_ages[1])
-        min_ages[which(min_ages > x)[1] - 1] + 1
-      else
-        0)
+  pm_rr_pp$age <- as.numeric(pm_rr_pp$age)
+  pm_rr_pp$ap_age <- 0
+  for(i in 1:length(min_ages)) pm_rr_pp$ap_age[pm_rr_pp$age>min_ages[i]] <- min_ages[i]+1
   
   pm_indices <- sapply(SCEN_SHORT_NAME,function(x)which(colnames(pm_rr_pp)==paste0("pm_conc_",x)))
   ### iterating over all all disease outcomes
@@ -21,11 +18,11 @@ gen_ap_rr <- function(pm_conc_pp){
     for (x in 1:length(SCEN_SHORT_NAME))
       pm_rr_pp[[paste0("RR_ap_", SCEN_SHORT_NAME[x])]] <- 1
     cause <- as.character(DISEASE_INVENTORY$ap_acronym[j])
-    dr_ap_disease <- subset(DR_AP, cause_code == cause)
+    dr_ap_disease <- DR_AP[DR_AP$cause_code == cause,]
     # apply by age groups
     ages <- unique(dr_ap_disease$age_code)
     for(age in ages){
-      dr_ap_sub <- subset(dr_ap_disease,age_code == age )
+      dr_ap_sub <- dr_ap_disease[dr_ap_disease$age_code == age,]
       if(age==99){
         i <-1:nrow(pm_rr_pp)
       }else{
