8bc5b9cbac477f2730f9468eeeb3941a7a64e92e
rahulatiitd <rahulatiitd@gmail.com>
2019-05-10 14:05:49 +0100

    created_whw_matrices

--- code/injuries/modify_injury_categories.R
+++ code/injuries/modify_injury_categories.R
@@ -1,20 +1,22 @@
 ##Sao Paulo
 ##reading the original file
-injuries_long_sao_paulo <- readRDS("C:/ITHIM-R/data/local/sao_paulo/injuries_long_sao_paulo.Rds")
+injuries_long_sao_paulo <- readRDS("data/local/sao_paulo/injuries_long_sao_paulo.Rds")
 ##saving the original file as backup
-saveRDS(injuries_long_sao_paulo, file = "C:/ITHIM-R/data/local/sao_paulo/injuries_long_sao_paulo_original.rds")
+saveRDS(injuries_long_sao_paulo, file = "data/local/sao_paulo/injuries_long_sao_paulo_original.rds")
 ##renaming ? as unk
 injuries_long_sao_paulo$strike_mode[which(injuries_long_sao_paulo$strike_mode =="?")]<- "unk"
-saveRDS(injuries_long_sao_paulo, file = "C:/ITHIM-R/data/local/sao_paulo/injuries_long_sao_paulo.rds")
+saveRDS(injuries_long_sao_paulo, file = "data/local/sao_paulo/injuries_long_sao_paulo.rds")
+
 
 ##Accra
 ##reading file with injury data
-accra<- readRDS('C:/ITHIM-R/data/local/accra/injuries_long_accra.RDS')
+accra<- readRDS('data/local/accra/injuries_long_accra.RDS')
 ##saving the original file as backup
-saveRDS(accra,'C:/ITHIM-R/data/local/accra/injuries_long_accra_original.RDS')
+saveRDS(accra,'data/local/accra/injuries_long_accra_original.RDS')
 accra$strike_mode[which(accra$strike_mode =="?")]<- "unk"
 ##saving the modified file
-saveRDS(accra,'C:/ITHIM-R/data/local/accra/injuries_long_accra.RDS')
+saveRDS(accra,'data/local/accra/injuries_long_accra.RDS')
+
 
 
 ##Delhi
@@ -29,3 +31,7 @@ write.csv(whw,paste0(path,'delhi_road_deaths_2010_2013_average.csv'))
 
 ##Bangalore
 ##Not needed
+whw_file <- 'bangalore_who_hit_who_matrix2011.csv'
+path <- file.path(find.package('ithimr',lib.loc=.libPaths()),'extdata/local/bangalore//')
+whw <- read.csv(paste0(path,whw_file))
+write.csv(whw,'results/injury_matrices/bangalore_whw.csv')
