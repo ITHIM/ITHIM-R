3a8520cda4e8d406aebc9987ffc003de032d9fda
usr110 <m.aliabbas@gmail.com>
2019-05-13 11:13:28 +0100

    Add people with trips

--- code/trips_analysis/multi-city-trips.R
+++ code/trips_analysis/multi-city-trips.R
@@ -54,3 +54,55 @@ for(city in cities){
   }
   
 }
+
+
+
+total_distance_trips <- list()
+total_duration_trips <- list()
+
+index <- 1
+
+# total_male_distance <- 
+
+groups <- c('All', "Male", "Female")
+
+for(city in cities){
+  for (groupings in groups){
+    if (groupings == 'All')
+      count_people <- length(unique(io[[city]]$trip_scen_sets$participant_id))
+    else if (groupings %in% c('Male', 'Female'))
+      count_people <- filter(io[[city]]$trip_scen_sets, sex == groupings) %>% distinct(participant_id) %>% nrow()
+    
+    td <- io[[city]]$trip_scen_sets %>% filter(scenario == 'Baseline') %>% 
+      group_by(stage_mode, scenario) %>% 
+      summarise(dur = round(sum(stage_distance) / count_people, 1)) %>% 
+      #mutate(grouping = groupings) %>%
+      spread(key = scenario, value = dur)
+    
+    names(td)[2] <- paste0(city, "_", groupings)
+    
+    if (index == 1){
+      total_distance_trips <- td
+    }else{
+      total_distance_trips <- full_join(total_distance_trips, td, by = "stage_mode")
+    }
+    
+    td <- io[[city]]$trip_scen_sets %>% filter(scenario == 'Baseline') %>% 
+      group_by(stage_mode, scenario) %>% 
+      summarise(dur = round(sum(stage_duration) / count_people, 1)) %>% #/ count_people
+      #mutate(grouping = groupings) %>%
+      spread(key = scenario, value = dur)
+    
+    names(td)[2] <- paste0(city, "_", groupings)
+    
+    if (index == 1){
+      total_duration_trips <- td
+    }else{
+      total_duration_trips <- full_join(total_duration_trips, td, by = "stage_mode")
+    }
+    
+    index <- index + 1
+    
+  }
+  
+}
