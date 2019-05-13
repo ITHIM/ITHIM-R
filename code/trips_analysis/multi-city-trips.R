a6defce45b217c06c31ff5626258fb15431caa08
usr110 <m.aliabbas@gmail.com>
2019-05-13 10:31:47 +0100

    Add script to generate trip distance and duration tables

---
+++ code/trips_analysis/multi-city-trips.R
@@ -0,0 +1,58 @@
+rm (list = ls())
+library(tidyverse)
+
+io <- readRDS("results/multi_city/io.rds")
+# Assumes that multi_city_script.R has been run till 
+cities <- c('accra','sao_paulo','delhi','bangalore')
+
+total_distance <- list()
+total_duration <- list()
+
+total_male_distance <- 
+  
+groups <- c('All', "Male", "Female")
+
+for(city in cities){
+  index <- 1
+  
+  for (groupings in groups){
+    
+    if (groupings == 'All')
+      count_people <- length(unique(io[[city]]$synth_pop$participant_id))
+    else if (groupings %in% c('Male', 'Female'))
+      count_people <- filter(io[[city]]$synth_pop, sex == groupings) %>% distinct(participant_id) %>% nrow()
+    
+    td <- io[[city]]$trip_scen_sets %>% filter(scenario == 'Baseline') %>% 
+      group_by(stage_mode, scenario) %>% 
+      summarise(dur = round(sum(stage_distance) / count_people, 1)) %>% 
+      mutate(grouping = groupings) %>%
+      spread(key = scenario, value = dur)
+    
+    if (index == 1){
+      total_distance <- td
+    }else{
+      total_distance <- full_join(total_distance, td, by = "stage_mode")
+    }
+    
+    td <- io[[city]]$trip_scen_sets %>% filter(scenario == 'Baseline') %>% 
+      group_by(stage_mode, scenario) %>% 
+      summarise(dur = round(sum(stage_duration) / count_people, 1)) %>% #/ count_people
+      mutate(grouping = groupings) %>%
+      spread(key = scenario, value = dur)
+    
+    if (index == 1){
+      total_duration <- td
+    }else{
+      total_duration <- full_join(total_duration, td, by = "stage_mode")
+    }
+    
+  }
+  
+  index <- index + 1
+  
+  
+}
+
+names(total_duration)[2:ncol(total_duration)] <- cities
+names(total_distance)[2:ncol(total_distance)] <- cities
+
