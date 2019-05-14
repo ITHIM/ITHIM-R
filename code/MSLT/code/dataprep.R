4d448065a54d7ca2a45fa70ff1e38e72ac863419
Belen <36194885+BelenZapata85@users.noreply.github.com>
2019-05-14 16:53:43 +1000

    Function for local GBD data
    GBD data is per localities in city regions, we need them separated to calculate population numbers

--- code/MSLT/code/dataprep.R
+++ code/MSLT/code/dataprep.R
@@ -32,8 +32,50 @@ options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
 
-### Prepare data for Bristol City Region
+### Prepare data for Bristol City Region (got data from 2007 to 2017 for trends, here only use 2017 data) 
+### Do the same per region if it works (then do a loop)
 
 gbd_bristol <- read.csv(file="code/MSLT/data/city regions/bristol/gbd_data_bristol.csv")
+as.integer(gbd_bristol$year)
+
+## May not need if loop works to create a list of localities by year 
+##gbd_bristol_2017 <- gbd_bristol[which(gbd_bristol$year=='2017'),]
+
+localities <- c('Bristol, City of', 'Bath and North East Somerset', 'North Somerset', 'South Gloucestershire')
+year <- c(2007: 2017)
+
+### Loop to create a data set for 2017 for each of the localities to calculate population numbers
+
+gbd_data_localities <- list()
+index <- 1
 
 
+for (l in localities){
+  for (y in year){
+    
+    region_data[[index]] <- sort_gbd_input(in_data = gbd_bristol, in_year = y, in_locality = l)
+    
+    index <- index + 1
+  }
+}
+    
+    
+
+disease_life_table_list_bl <- list()
+index <- 1
+
+for (age in i_age_cohort){
+  for (sex in i_sex){
+    for (disease in i_disease) {
+      # Exclude bc for Males
+      if (sex == "male" && disease == "bc"){
+        # cat("\n") #Uncomment to see list
+      }
+      else {
+        # cat("age ", age, " sex ", sex, "and disease", disease, "\n") #Uncomment to see list
+        disease_life_table_list_bl[[index]] <- run_disease(in_idata = mslt_df, in_sex = sex, in_mid_age = age, in_disease = disease)
+        index <- index + 1
+      }
+    }
+  }
+}
