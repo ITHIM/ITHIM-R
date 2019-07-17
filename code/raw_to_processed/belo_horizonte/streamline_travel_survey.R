6377e464d565b28efe6f49464826fde00ccc0924
usr110 <m.aliabbas@gmail.com>
2019-07-17 20:37:44 +0100

    Add walk_to_pt for bus and metro

--- code/raw_to_processed/belo_horizonte/streamline_travel_survey.R
+++ code/raw_to_processed/belo_horizonte/streamline_travel_survey.R
@@ -6,7 +6,7 @@ library(tidyverse)
 library(summarytools)
 
 # Read belo horizonte's travel survey
-rd <- read_csv("data/local/belo_horizonte/trips_belo_horizonte.csv")
+rd <- read_csv("data/local/belo_horizonte/trips_dataset_LT_170719.csv")
 
 # Remove unused column
 rd$X1 <- NULL
@@ -16,7 +16,8 @@ rd <- rd %>% rename(participant_id = id_person,
                     age = Idade,
                     sex = female,
                     trip_id = id_trip,
-                    stage_mode = mode_eng
+                    stage_mode = mode_eng,
+                    stage_duration = trip_duration
                     )
 
 # Convert factor to appropriate classes
@@ -25,10 +26,26 @@ rd <- mutate(rd, participant_id = as.integer(as.factor(participant_id)),
              trip_id = as.integer(as.factor(trip_id)))
 
 # Show mode share (%)
-rd %>% group_by(stage_mode) %>% summarise(share = (n() / nrow(.) * 100) %>% round(2))
+rd %>% group_by(stage_mode) %>% summarise(count = n(), `share (%)` = (n() / nrow(.) * 100) %>% round(2))
 
 # Show mode duration (minutes)
-rd %>% group_by(stage_mode) %>% summarise(mean_duration = mean(trip_duration) %>% round(2))
+rd %>% group_by(stage_mode) %>% summarise(mean_duration = mean(stage_duration) %>% round(2))
 
 # View summary of people without any trips
 view(dfSummary(rd %>% filter(is.na(trip_id))))
+
+# Add stage_id
+rd <- rd %>% mutate(stage_id = 1)
+
+# Select pt trips
+rd_pt <- rd %>% filter(stage_mode %in% c('bus', 'metro'))
+
+# Add short walks 
+rd_pt_short_walk <- rd_pt %>% mutate(stage_mode = "walk_to_pt", stage_id = 2, stage_duration = 10.55)
+
+rd <- plyr::rbind.fill(rd, rd_pt_short_walk)
+
+# Calculate trip distance by summing all stages' distance
+rd$trip_duration <- ave(rd$stage_duration, rd$trip_id, FUN=sum)
+
+rd <- arrange(rd, trip_id)
