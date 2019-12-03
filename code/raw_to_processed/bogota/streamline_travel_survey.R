02ac2f0d3fd233ba61887806e933d730ad07db4a
Ali Abbas <m.aliabbas@gmail.com>
2019-12-03 14:53:54 +0000

    Fix bug to rename walk to walk to pt

--- code/raw_to_processed/bogota/streamline_travel_survey.R
+++ code/raw_to_processed/bogota/streamline_travel_survey.R
@@ -63,8 +63,10 @@ rdpt <- rbind(rdpt, distinct_rdpt)
 # Add pt trips
 rd <- rbind(rd %>% filter(trip_mode != c('bus', 'train') | is.na(trip_mode)), rdpt)
 
+
 # Rename walk to walk_to_pt for pt modes
-rd[rd$trip_mode %in% c("bus", "train") & rd$stage_mode == "walk",] <- "walk_to_pt"
+if (nrow(rd[rd$trip_mode %in% c("bus", "train") && rd$stage_mode == "walk",]) > 0)
+  rd[rd$trip_mode %in% c("bus", "train") && rd$stage_mode == "walk",]$stage_mode <- "walk_to_pt"
 
 # Arrange df
 rd <- arrange(rd, participant_id, trip_id, stage_id)
