7b1bcfb9d2e0ee60c34bf0a3c332eb88f27e3f9f
Ali Abbas <m.aliabbas@gmail.com>
2019-12-11 11:31:11 +0000

    Expand and print mode share summary

--- code/raw_to_processed/visakhapatnam/streamline_travel_survey.R
+++ code/raw_to_processed/visakhapatnam/streamline_travel_survey.R
@@ -5,5 +5,11 @@ rm (list = ls())
 library(tidyverse)
 library(summarytools)
 
-# Read santiago's travel survey
+# Read visakhapatnam's travel survey
 raw_rd <- read_csv("data/local/vizag/visakhapatnam_trips.csv")
+
+# Expand dataset by wk_freq
+rd <- raw_rd %>% mutate(wk_freq = if_else(is.na(wk_freq), 1, wk_freq)) %>% uncount(wk_freq, .id = "trip_id")
+
+# Print mode share
+rd %>% filter(!is.na(mode)) %>% group_by(mode, .drop = F) %>% summarise(mode_share = round(n()*100/nrow(.),1))
