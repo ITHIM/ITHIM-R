d58364da600d9400de98f23e634260bd26096daa
Rob Johnson <rob.johnson@mrc-bsu.cam.ac.uk>
2019-12-29 08:51:20 +0000

    4096 samples

--- multi_city_voi.R
+++ multi_city_voi.R
@@ -120,7 +120,7 @@ save(cities,setting_parameters,injury_reporting_rate,chronic_disease_scalar,pm_c
 parameters_only <- F
 multi_city_ithim <- outcome <- outcome_pp <- yll_per_hundred_thousand <- list()
 numcores <- 16
-nsamples <- 1024
+nsamples <- 4096
 print(system.time(
   for(ci in 1:length(cities)){
     city <- cities[ci]
