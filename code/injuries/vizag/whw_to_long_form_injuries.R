fe8261af3ad9d10c41de102984a0cd320b23db7d
Ali Abbas <m.aliabbas@gmail.com>
2020-01-07 16:17:34 +0000

    Correct output filename

--- code/injuries/vizag/whw_to_long_form_injuries.R
+++ code/injuries/vizag/whw_to_long_form_injuries.R
@@ -13,15 +13,21 @@ for(i in 1:nrow(whw))
 injuries$strike_mode[injuries$strike_mode=='X2.3.Wheeled'] <- 'motorcycle'
 injuries$strike_mode[injuries$strike_mode=='Car.pick.up.van'] <- 'car'
 injuries$strike_mode[injuries$strike_mode=='Trucks'] <- 'truck'
-injuries$strike_mode[injuries$strike_mode=='Pedal.cycle'] <- 'bicycle'
+injuries$strike_mode[injuries$strike_mode=='Bus'] <- 'bus'
+
+
 injuries$cas_mode[injuries$cas_mode=='Pedal Cycle'] <- 'bicycle'
 injuries$cas_mode[injuries$cas_mode=='3Wheeled'] <- 'auto_rickshaw'
 injuries$cas_mode[injuries$cas_mode=='Heavy transport'] <- 'truck'
+
+injuries$cas_mode <- tolower(injuries$cas_mode)
+injuries$strike_mode <- tolower(injuries$strike_mode)
+
 injuries
 head(injuries)
 unique(injuries$cas_mode)
 unique(injuries$strike_mode)
-injury_file <- 'injuries_delhi.csv'
+injury_file <- 'injuries_vizag.csv'
 write.csv(injuries,paste0('inst/extdata/local/vizag/',injury_file))
 
 
