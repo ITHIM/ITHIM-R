cb7f6e9c22e4edee869b3f61d09bda499ed966e6
Rob Johnson <rob.johnson@mrc-bsu.cam.ac.uk>
2020-01-22 13:50:48 +0000

    fix nov model

--- R/distances_for_injury_function.R
+++ R/distances_for_injury_function.R
@@ -69,7 +69,7 @@ distances_for_injury_function <- function(trip_scen_sets,dist){
   CAS_EXPONENT <<- SIN_EXPONENT_SUM * CASUALTY_EXPONENT_FRACTION
   STR_EXPONENT <<- SIN_EXPONENT_SUM - CAS_EXPONENT
   forms <- list(whw='count~cas_mode*strike_mode+offset(log(cas_distance)+log(strike_distance)+(CAS_EXPONENT-1)*log(cas_distance_sum)+(STR_EXPONENT-1)*log(strike_distance_sum)-log(injury_reporting_rate)+log(weight))',
-                nov='count~cas_mode+offset(2*CAS_EXPONENT*log(cas_distance)-log(injury_reporting_rate)+log(weight))')
+                nov='count~cas_mode+offset(CAS_EXPONENT*log(cas_distance)-log(injury_reporting_rate)+log(weight))')
   if('age_cat'%in%names(injuries_for_model[[1]][[1]]))
     for(type in INJURY_TABLE_TYPES)
       forms[[type]] <- paste0(c(forms[[type]],'age_cat'),collapse='+')
