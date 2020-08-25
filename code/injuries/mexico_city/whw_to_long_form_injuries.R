9f317db0586da04423e8fe5077446e6f994c879c
usr110 <m.aliabbas@gmail.com>
2020-08-25 14:27:32 +0100

    Update code for the new structure of whw matrix

--- code/injuries/mexico_city/whw_to_long_form_injuries.R
+++ code/injuries/mexico_city/whw_to_long_form_injuries.R
@@ -1,17 +1,14 @@
 library(ithimr)
 file_path <- file.path('data/local/mexico_city/mexico city injury data updated using bogota.csv')
 whw <- read_csv(file_path)
-injuries <- data.frame(cas_mode=character(),strike_mode=character(),weight=numeric(),stringsAsFactors = F)
+injuries <- data.frame(cas_mode=character(),strike_mode=character(),stringsAsFactors = F)
 for(i in 1:nrow(whw))
   for(j in 2:ncol(whw)){
-    count <- whw[i,j]
-    if(count>0){
-      weight <- number_of_years*ceiling(count)/count
-      for(k in 1:ceiling(count)){ 
-        #print(c(k,count))
-        injuries[nrow(injuries)+1,] <- c(as.character(whw[i,1]),colnames(whw)[j],weight)
+    count <- ceiling(whw[i,j]) %>% as.numeric()
+    if(count>0)
+      for(k in 1:count){
+        injuries[nrow(injuries)+1,] <- c(as.character(whw[i,1]),colnames(whw)[j])
       }
-    }
   }
 
 
