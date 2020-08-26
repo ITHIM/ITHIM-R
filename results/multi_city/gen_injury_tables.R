89d3c743ed26abed43c758975fef1198f5f09bc4
Ali Abbas <m.aliabbas@gmail.com>
2020-08-26 17:32:21 +0100

    Add script to generate injury tables for whw, and injury risk by population and distance

---
+++ results/multi_city/gen_injury_tables.R
@@ -0,0 +1,75 @@
+############ Load libraries and datasets
+require(tidyverse)
+require(flextable)
+whw <- read_csv("results/multi_city/whw_matrices/whw_lng.csv")
+injury_risks_per_100k <- read_csv("results/multi_city/whw_matrices/injury_risks_per_100k_pop.csv")
+injury_risks_per_billion_kms <- read_csv('results/multi_city/whw_matrices/injury_risks_per_billion_kms_lng.csv')
+
+# Read lookup table
+smodes <- read_csv('data/global/modes/standardized_modes.csv')
+
+get_summary_table <- function(mode = 'strike', scen = scen, mode_name){
+  if (mode == 'strike'){
+    
+    temp1 <- whw %>% filter(scenario == scen & str_mode == mode_name) %>% spread(value = value, key = cas_mode)
+    
+    temp1 <- temp1[,c(1, 2, 3, na.omit(match(smodes$exhaustive_list, colnames(temp1))))]
+    
+    return(temp1 %>% mutate_if(is.numeric, .funs = funs(case_when( . < 1 ~ round(., 2) ,  . >= 1 ~  as.numeric(round(.))))) %>%  mutate_if(is.numeric, ~as.character(.)))
+    
+  }else if (mode == 'casualty') {
+    
+    temp1 <- whw %>% filter(scenario == scen & cas_mode == mode_name) %>% spread(value = value, key = str_mode)
+    
+    temp1 <- temp1[,c(1, 2, 3, na.omit(match(smodes$exhaustive_list, colnames(temp1))))]
+    
+    return(temp1 <- temp1 %>% mutate_if(is.numeric, .funs = funs(case_when( . < 1 ~ round(., 2) ,  . >= 1 ~  as.numeric(round(.))))) %>%  mutate_if(is.numeric, ~as.character(.)))
+    
+    
+  }
+}
+
+get_summary_table_injury_risk <- function(obj = injury_risks_per_100k, mode = 'city', var = var){
+  if (mode == 'scen'){
+  td <- obj %>% filter(scenario == var) %>% spread(value = value, key = mode)
+  return(td[,c(1, 2, na.omit(match(smodes$exhaustive_list, colnames(td))))] %>% 
+    mutate_if(is.numeric, .funs = funs(case_when( . < 1 ~ round(., 2) ,  . >= 1 ~  as.numeric(round(.))))) %>%  mutate_if(is.numeric, ~as.character(.)))
+  }else if (mode == 'city'){
+    td <- obj %>% filter(city == var) %>% spread(value = value, key = mode)
+    return(td[,c(1, 2, na.omit(match(smodes$exhaustive_list, colnames(td))))] %>% 
+             mutate_if(is.numeric, .funs = funs(case_when( . < 1 ~ round(., 2) ,  . >= 1 ~  as.numeric(round(.))))) %>%  mutate_if(is.numeric, ~as.character(.)))
+  }
+  
+  
+}
+
+############ Define scenario
+scen_names <- unique(whw$scenario)
+scen <- 'Baseline'
+temp <- whw %>% filter(scenario == scen) %>% dplyr::select(cas_mode)
+cas_mode <- unique(temp$cas_mode)
+cm <- cas_mode[1]
+cm <- 'bus'
+
+temp <- whw %>% filter(scenario == scen) %>% dplyr::select(str_mode)
+str_mode <- unique(temp$str_mode)
+sm <- str_mode[1]
+sm <- 'unknown'
+
+cn <- 'delhi'
+
+get_summary_table(mode = 'casualty', scen = scen, mode_name = cm ) %>% View()
+
+get_summary_table(mode = 'strike', scen = scen, mode_name = sm ) %>% View()
+
+# ft <- flextable()
+# ft <- add_header_row(ft, values = c(' ', 'strike mode'), colwidths = c(3, ncol(temp1) - 3))
+# ft <- merge_v(ft, j = 1:3, part = 'body')
+# 
+# ft
+
+##################
+
+get_summary_table_injury_risk(injury_risks_per_100k, mode = 'scen', var = scen) %>% View()
+
+get_summary_table_injury_risk(injury_risks_per_billion_kms, mode = 'city', var = cn) %>% View()
