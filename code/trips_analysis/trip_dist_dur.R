17088f501e40d9128a0774c5c973039b02c54230
Ali Abbas <m.aliabbas@gmail.com>
2020-10-13 16:08:27 +0100

    Code to see if dist and dur are already part of the trip dataset

---
+++ code/trips_analysis/trip_dist_dur.R
@@ -0,0 +1,34 @@
+require(tidyverse)
+rm(list=ls())
+# io <- readRDS("results/multi_city/io.rds")
+# Assumes that multi_city_script.R has been run till 
+# Get names of cities from the io object
+cities <- c('accra', 'bangalore', 'belo_horizonte', 'bogota', 'buenos_aires', 'cape_town',
+            'delhi', 'mexico_city', 'santiago', 'sao_paulo', 'vizag')
+i <- 1
+
+l <- list()
+for (city in cities){
+  trip_set <- read.csv(paste0('inst/extdata/local/',city,'/trips_', city, '.csv'), stringsAsFactors = F)
+  l[[city]] <- trip_set
+  
+  dist <- trip_set %>% dplyr::select(ends_with('duration')) %>% length()
+  dur <- trip_set %>% dplyr::select(ends_with('duration')) %>% length()
+  
+    if(dist && dur){
+      
+      print(paste(i, city , 'has both distance and duration'))
+      
+    }else if(dist && !dur){
+      
+      print(paste(i, city , 'has ONLY distance'))
+      
+    }else if(!dist && dur){
+     
+      print(paste(i, city , 'has ONLY duration'))
+      
+    }
+  
+  i <- i + 1
+  
+}
