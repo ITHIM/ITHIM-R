4611bfc4811c793c9943973fcef83f56399b106c
Ali Abbas <m.aliabbas@gmail.com>
2020-10-14 11:44:30 +0100

    Use list

--- code/trips_analysis/trip_dist_dur.R
+++ code/trips_analysis/trip_dist_dur.R
@@ -8,6 +8,8 @@ cities <- c('accra', 'bangalore', 'belo_horizonte', 'bogota', 'buenos_aires', 'c
 i <- 1
 
 l <- list()
+
+speed_df <- list()
 for (city in cities){
   trip_set <- read.csv(paste0('inst/extdata/local/',city,'/trips_', city, '.csv'), stringsAsFactors = F)
   l[[city]] <- trip_set
@@ -18,6 +20,21 @@ for (city in cities){
     if(dist && dur){
       
       print(paste(i, city , 'has both distance and duration'))
+      
+      lsdf <- trip_set %>% mutate(speed = stage_distance / ( stage_duration / 60)) %>% group_by(stage_mode) %>% 
+        summarise(median_speed = median(speed, na.rm = T), mean_speed = mean(speed, na.rm = T))
+      lsdf$city <- city
+      
+      
+      if (length(speed_df) > 0) {
+        speed_df <- plyr::rbind.fill(speed_df, lsdf)
+            
+      }else{
+        
+        speed_df <- lsdf
+        
+      }
+      
       
     }else if(dist && !dur){
       
