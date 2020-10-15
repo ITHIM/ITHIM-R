340712f584de49832e10c8616c58527e1e099dab
Ali Abbas <m.aliabbas@gmail.com>
2020-10-15 12:23:11 +0100

    Add comments

--- code/trips_analysis/trip_dist_dur.R
+++ code/trips_analysis/trip_dist_dur.R
@@ -1,31 +1,48 @@
+# Load library
 require(tidyverse)
+
+# Clear workspace
 rm(list=ls())
-# io <- readRDS("results/multi_city/io.rds")
-# Assumes that multi_city_script.R has been run till 
-# Get names of cities from the io object
+
+# Cities names
 cities <- c('accra', 'bangalore', 'belo_horizonte', 'bogota', 'buenos_aires', 'cape_town',
             'delhi', 'mexico_city', 'santiago', 'sao_paulo', 'vizag')
 i <- 1
 
+# List to hold trip datasets
 l <- list()
 
+# List to save all stage and trip speed data
 speed_df <- list()
 for (city in cities){
   trip_set <- read.csv(paste0('inst/extdata/local/',city,'/trips_', city, '.csv'), stringsAsFactors = F)
   l[[city]] <- trip_set
   
+  # Check if distance column exists
   dist <- trip_set %>% dplyr::select(ends_with('distance')) %>% length()
+  # Check if duration column exists
   dur <- trip_set %>% dplyr::select(ends_with('duration')) %>% length()
   
   if(dist && dur){
     
+    # Local df for saving speed data
     lsdf <- NULL
     
     print(paste(i, city , 'has both distance and duration'))
+    
+    # Subset trip data with both dist and duration
+    # calculate speed
+    # group by mode
+    # calculate mean and median
+    
+    # If stage data is available
     if (any(names(trip_set) %in% 'stage_duration') && (any(names(trip_set) %in% 'stage_distance'))){
-      lsdf <- trip_set %>% dplyr::filter(stage_distance > 0 & stage_duration > 0 & !is.na(stage_mode)) %>%  mutate(speed = stage_distance / ( stage_duration / 60)) %>% group_by(stage_mode) %>% 
+      lsdf <- trip_set %>% dplyr::filter(stage_distance > 0 & stage_duration > 0 & !is.na(stage_mode)) %>%  
+        mutate(speed = stage_distance / ( stage_duration / 60)) %>% group_by(stage_mode) %>% 
         summarise(median_speed = median(speed, na.rm = T), mean_speed = mean(speed, na.rm = T))
       lsdf$city <- city
+      
+      # If not, then use trip data
     }else if (any(names(trip_set) %in% 'trip_duration') && (any(names(trip_set) %in% 'trip_distance'))){
       lsdf <- trip_set %>% dplyr::filter(trip_distance > 0 & trip_duration > 0 & !is.na(trip_mode)) %>%  mutate(speed = trip_distance / ( trip_duration / 60)) %>% group_by(trip_mode) %>% 
         summarise(median_speed = median(speed, na.rm = T), mean_speed = mean(speed, na.rm = T))
@@ -36,11 +53,13 @@ for (city in cities){
     
     if (!is.null(lsdf)){
       
+      # Bind speed data
       if (length(speed_df) > 0) {
         speed_df <- plyr::rbind.fill(speed_df, lsdf)
         
       }else{
         
+        # Initial speed data
         speed_df <- lsdf
         
       }
