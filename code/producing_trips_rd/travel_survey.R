22bc6c552267224c6a3b320662c1ae9dfbc6e9fb
Ali Abbas <m.aliabbas@gmail.com>
2020-11-23 16:01:28 +0000

    Comment out expansion for weekly trips for Vizag

--- code/producing_trips_rd/travel_survey.R
+++ code/producing_trips_rd/travel_survey.R
@@ -2308,17 +2308,12 @@ trip <- standardize_modes(trip, mode = c('trip'))
 # rd <- expand_using_weights(trip)
 rd <- trip
 
-# Expand dataset by trip_wkly_frequency
-# Add trip and stage id
-rd <- rd %>% mutate(trip_wkly_frequency = if_else(is.na(trip_wkly_frequency), 1, trip_wkly_frequency)) %>% uncount(trip_wkly_frequency, .id = "tid") %>% 
-  mutate(trip_id = as.integer(rownames(.)))
-
 # Remove extra columns
 rd$X1 <- NULL
 
 rd$participant_id <- as.integer(as.factor(with(rd, paste(cluster_id, household_id, participant_id, sep = "_"))))
 
-rd$trip_id <- as.integer(as.factor(with(rd, paste(cluster_id, household_id, participant_id, trip_id, tid, sep = "_"))))
+rd$trip_id <- as.integer(as.factor(with(rd, paste(cluster_id, household_id, participant_id, trip_id, sep = "_"))))
 
 # Reorder and select columns
 rd1 <- rd %>% dplyr::select(participant_id, age, sex, trip_id, trip_mode, trip_duration, trip_distance)
