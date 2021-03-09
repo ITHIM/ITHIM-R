835c8ba44bcfebf234a59751537d08f769e8c9db
Daniel Gil <danielgils@gmail.com>
2021-03-09 06:40:52 -0500

    Imputation of modes in CapeTown
    injuries_cape_town_original.csv is the original file
    injuries_cape_town_NAs is the file with unknowns
    injuries_cape_town.csv is the file with imputed data

--- code/injuries/cape_town/raw_to_processed_injuries.R
+++ code/injuries/cape_town/raw_to_processed_injuries.R
@@ -1,5 +1,6 @@
 # Load libraries
 library(tidyverse)
+library(mice)
 
 # Set filepath
 file_path <- file.path('data/local/cape_town/injuries_cape_town.csv')
@@ -25,7 +26,59 @@ smodes <- smodes %>% separate_rows(original, sep = ';')
 smodes <- smodes %>% 
   mutate(across(where(is.character), str_trim))
 
+# Multiple imputation using MICE
+# https://stats.idre.ucla.edu/r/faq/how-do-i-perform-multiple-imputation-using-predictive-mean-matching-in-r/
+whw2 <- whw %>% mutate(
+  cas_mode = factor(ifelse(cas_mode == "unknown", NA, cas_mode)),
+  strike_mode = factor(ifelse(strike_mode == "unknown", NA, strike_mode))
+)
+
+# There are 2 age missing, 47 cas_mode and 141 strike mode missing.
+md.pattern(whw2)
+
+# Out of the 47 and 151 cas and strike mode, in only 3 rows both modes are missing
+md.pairs(whw2)
+
+# imputation
+imp1 <- mice(whw2, m = 100, seed = 12345)
+imp1
+#names(imp1$imp$cas_age)
+
+# Save object with imputation
+saveRDS(imp1, file = "code/injuries/cape_town/imputed.RDS") 
+# imp1 <- readRDS("code/injuries/cape_town/imputed.RDS")
+
+# Create the function to get the mode, from here https://www.tutorialspoint.com/r/r_mean_median_mode.htm
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+# Impute mode in age
+cas_age <- imp1$imp$cas_age %>% rownames_to_column("id") 
+cas_age$impute <- apply(cas_age[2:ncol(cas_age)], 1, getmode)
+
+# Impute mode in cas_mode
+cas_mode <- imp1$imp$cas_mode %>% rownames_to_column("id") 
+cas_mode$impute <- apply(cas_mode[2:ncol(cas_mode)], 1, getmode)
+
+# Impute mode in strike_mode
+strike_mode <- imp1$imp$strike_mode %>% rownames_to_column("id") 
+strike_mode$impute <- apply(strike_mode[2:ncol(strike_mode)], 1, getmode)
+
+whw3 <- whw2
+whw3[cas_age$id, "cas_age"] <- cas_age$impute
+whw3[cas_mode$id, "cas_mode"] <- cas_mode$impute
+whw3[strike_mode$id, "strike_mode"] <- strike_mode$impute
+
+
+# Check these values were indeed imputed
+# summary(whw2$cas_age); summary(whw3$cas_age)
+# table(whw2$cas_mode, useNA = "always");table(whw3$cas_mode, useNA = "always")
+# table(whw2$strike_mode, useNA = "always");table(whw3$strike_mode,useNA = "always")
+
 # Recode cas mode
+whw <- whw3
 whw$cas_mode <- smodes$exhaustive_list[match(tolower(whw$cas_mode), smodes$original)]
 
 # Recode strike mode
