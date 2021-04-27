b319d3ed2f6bad119d59df73c6565913124c76a5
rahulatiitd <rahulatiitd@gmail.com>
2021-04-27 11:56:53 +0530

    Accra injury processing using Rob's dataset

--- code/injuries/accra/processing_raw_injury_data_accra.R
+++ code/injuries/accra/processing_raw_injury_data_accra.R
@@ -3,8 +3,33 @@ library(tidyverse)
 library(reshape2)
 library(mice)
 
-# Specify cities
-cities <- c('accra')
+# Specify path 
+path<- file.path('data/local/accra/')
+
+##reading dataframe with road deaths victims
+whw<- readRDS(paste0(path, '/accra_injuries_dataset_rob.Rds'))
+
+whw$victim_strike_pair<- paste(whw$cas_mode, "-", whw$strike_mode)
+names(whw)[7]<- "combo"
+#write.csv(unique(accra$victim_strike_pair), 'unique_victim_strike_pairs.csv')
+lookup<- read.csv(paste0(path,'/unique_victim_strike_pairs.csv'))
+icd_codes<-  read.csv(paste0(path,'/icd_code_look_up_table_row_column.csv'))
+whw<- whw %>% left_join(lookup, "combo")
+whw<- whw %>% left_join(icd_codes, by ="ICD")
+strik_list <- c('No other/fixed or stationary object'	,'Pedestrian',	
+                'Pedal cycle', '2/3 Wheeled',	'Car/pick-up/van', 'Bus',
+                'Railway train/railway vehicle', 'non-motor vehicle',	
+                'unspecified', 'Non-collision', 'Unknown', 'Trucks')
+vict_list <- c('Pedestrian'	,'Pedal Cycle',	'Motorcycle',	'3Wheeled',	'Car',
+               'Pick-up truck/van',	'Heavy transport',	'Bus'	,'Other',	
+               'Unknown',	'Railway')
+whw$cas_type <- vict_list[whw$row]
+whw$strk_type <- strik_list[whw$column]
+
+whw<- subset(whw, select=c("year","cas_age", "cas_gender", "cas_type", "strk_type"))
+whw$weight<- 10
+
+####################
 
 # Read lookup table for standardized modes
 smodes <- read_csv('data/global/modes/standardized_modes.csv')
@@ -13,88 +38,9 @@ smodes <- smodes %>% separate_rows(original, sep = ';')
 # Trim
 smodes <- smodes %>% mutate(across(where(is.character), str_trim))
 
-# Create a list for all whw matrices - used for debugging
-temp <- list()
-
-# Loop
-#for (city in cities){
-city = cities # Because it's only accra 
-  
-  # Specify path 
-  path <- file.path(paste0('data/local/',city, '/', city, '_injuries.csv'))
-  
-  # Read whw matrix
-  whw <- read_csv(path)
-  
-  # Check if n_years exist
-  weight <- 1
-  
-  if (any(names(whw) == 'n_years')){
-    weight <- unique(whw$n_years)
-    # remove n_years column
-    whw$n_years <- NULL
-  }
-  
-  # Format to long form
-  whw_lng <- reshape2::melt(whw)
-  
-  # Remove all values with NA as value
-  whw_lng <- whw_lng %>% filter(!is.na(value))
-  
-  # Repeat rows with cieled count
-  whw_lng <- whw_lng[rep(1:nrow(whw_lng), ceiling(whw_lng$value)),1:2] %>% as.data.frame()
-  
-  # Rename columns
-  names(whw_lng)[1:2] <- c('cas_mode', 'strike_mode')
-  
-  # Change case to lower
-  whw_lng$cas_mode <- tolower(whw_lng$cas_mode)
-  
-  # Change case to lower
-  whw_lng$strike_mode <- tolower(whw_lng$strike_mode)
-  
-  # Print useful info
-  print(city)
-  print('cas mode')
-  print(unique(whw_lng$cas_mode))
-  print(unique(whw_lng$cas_mode) %in% smodes$original)
-  print('strike mode')
-  print(unique(whw_lng$strike_mode))
-  print(unique(whw_lng$strike_mode) %in% smodes$original)
-  
-  # If any of the cas_mode is not in the standardized original mode, break the loop
-  if(!any(unique(whw_lng$cas_mode) %in% smodes$original)){
-    print(unique(whw_lng$cas_mode))
-    break
-  }
-  
-  # If any of the strike_mode is not in the standardized original mode, break the loop
-  if(!any(unique(whw_lng$strike_mode) %in% smodes$original)){
-    print(unique(whw_lng$strike_mode))
-    break
-  }
-  
-  # Check if there are unknown or unspecified modes
-  sum(is.na(whw_lng$cas_mode))
-  sum(is.na(whw_lng$strike_mode))
-  
-  # Since there are no unknown or unspecified modes, there's no need to impute
-  
-  # Recode cas mode to standard mode
-  whw_lng$cas_mode <- smodes$inj_vic_lng[match(tolower(whw_lng$cas_mode), smodes$original)]
-  
-  # Recode strike mode to standard mode
-  whw_lng$strike_mode <- smodes$inj_str_lng[match(tolower(whw_lng$strike_mode), smodes$original)]  
-  
-  # Add weight column
-  whw_lng$weight <- weight
-  
-  # Save whw_lng in list
-  temp[[city]] <- whw_lng
-  
   # Specify name of output file
   injury_file <- paste0('injuries_', city, '.csv')
   
   # Write file to the right folder
-  write_csv(whw_lng, paste0('inst/extdata/local/',city, '/', injury_file))
+  write_csv(whw, paste0('inst/extdata/local/',city, '/', injury_file))
 #}
