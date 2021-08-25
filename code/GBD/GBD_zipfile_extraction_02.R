0eb985705aed67d5cd7e944ee7a7e4bef7b351a0
Daniel Gil <danielgils@gmail.com>
2021-08-25 18:27:03 -0500

    Updating script to extract countries from GBD data 2019

--- code/GBD/GBD_zipfile_extraction_02.R
+++ code/GBD/GBD_zipfile_extraction_02.R
@@ -3,20 +3,33 @@
 #and finally deleiting extracted files. Resulting dataframe is then saved as csv-file.
 
 #Created in February-March 2019 by Marko Tainio
+# Modified in August 2021 by Daniel Gil
+
+rm(list = ls())
 
 #Defining folder where the data is
-data_folder <- "V:\\Studies\\MOVED\\HealthImpact\\Data\\Global_Burden_Disease_2017\\"
+#data_folder <- "V:/Studies/MOVED/HealthImpact/Data/Global/Global_Burden_Disease_2019/"
+#temp_folder <- paste0(data_folder,"temp")
+#result_folder <- "V:/Studies/MOVED/HealthImpact/Projects/TIGTHAT/Case cities data/GBD 2019 data extraction/"
+#gbdfile_name <- "IHME-GBD_2019_DATA-b1e8e391-"
+
+# I am running this locally because it is faster
+# If we want to run from v-drive just uncomment lines 9-12
+data_folder <- "C:/Users/danie/Documents/Daniel_Gil/Consultorias/2021/Cambridge/Data/GBD/2019/CountriesAndTerritoriesAndSubregions/"
 temp_folder <- paste0(data_folder,"temp")
-result_folder <- "V:\\Studies\\MOVED\\HealthImpact\\Research\\TIGTHAT\\Case cities data\\GBD 2017 data extraction\\"
-gbdfile_name <- "IHME-GBD_2017_DATA-2d3c1979-"
+result_folder <- "C:/Users/danie/Documents/Daniel_Gil/Consultorias/2021/Cambridge/Data/GBD/2019/GBD 2019 data extraction/"
+gbdfile_name <- "IHME-GBD_2019_DATA-b1e8e391-"
 
 #Next two lines defines countries that will be extracted
+# Dan: In the file "countries to be extracted I added Uruguay, Canada, USA, Peru
+# and New York because we have new data for these cities.
 countries <- read.csv((paste0(result_folder, "Countries to be extracted.csv")))
 countries <- unlist(countries[,2])
 
 data_extracted <- NULL
 
-for (i in 1:91) {
+for (i in 1:79) {
+  print(paste0("folder: ", i))
   file_number <- i
   
   file_select <- paste0(data_folder,gbdfile_name, i,".zip")
@@ -27,11 +40,13 @@ for (i in 1:91) {
   file.remove(paste0(temp_folder,"\\", gbdfile_name, i, ".csv"))
   data_read <- subset(data_read, location_id %in% countries)
 
-  data_extracted <-rbind(data_extracted,data_read)
+  data_extracted <- rbind(data_extracted,data_read)
 }
 
-unlink(paste0(temp_folder), recursive = TRUE)
-write.csv(data_extracted, file =paste0(result_folder, "GBD2017_countries_extracted.csv"), row.names=FALSE)
+#unlink(paste0(temp_folder), recursive = TRUE)
+write.csv(data_extracted, 
+          file = paste0(result_folder, "GBD2019_countries_extracted.csv"), 
+          row.names = FALSE)
 
 rm(data_folder, temp_folder, result_folder, gbdfile_name, countries, data_extracted, file_number, i, data_read, file_select)
 
