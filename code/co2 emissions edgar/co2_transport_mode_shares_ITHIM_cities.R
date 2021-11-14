db5ea9e1954a51758d5595025861ca4a4628e6c7
Daniel Gil <danielgils@gmail.com>
2021-11-14 11:23:13 -0500

    Updating code for other cities

--- code/co2 emissions edgar/co2_transport_mode_shares_ITHIM_cities.R
+++ code/co2 emissions edgar/co2_transport_mode_shares_ITHIM_cities.R
@@ -4,66 +4,83 @@ library(tidyr)
 library(rgdal)
 library(sp)
 library(sf)
-setwd('code/co2 emissions edgar/')
+
+rm(list = ls())
+#setwd('code/co2 emissions edgar/')
+path <- 'code/co2 emissions edgar/'
+
 
 ##reading CO2 files specific to each mode of transport
-bus<- read.table('txt_TRO_TNR/monica_v5.0_EM_CO2_TRO_ROA_BS0_2012_CO2_2012.txt', header = T, sep=";")
-bus$mode<- "bus"
-all_modes<- bus
+bus <- read.table(paste0(path, 'txt_TRO_TNR/monica_v5.0_EM_CO2_TRO_ROA_BS0_2012_CO2_2012.txt'),
+                         header = T, sep = ";")
+bus$mode <- "bus"
+all_modes <- bus
 
-htrucks<- read.table('txt_TRO_TNR/monica_v5.0_EM_CO2_TRO_ROA_HD0_2012_CO2_2012.txt', header = T, sep=";")
-htrucks$mode<- "htrucks"
-all_modes<- rbind(all_modes, htrucks)
+htrucks <- read.table(paste0(path, 'txt_TRO_TNR/monica_v5.0_EM_CO2_TRO_ROA_HD0_2012_CO2_2012.txt'), 
+                     header = T, sep = ";")
+htrucks$mode <- "htrucks"
+all_modes <- rbind(all_modes, htrucks)
 
-lttrucks<- read.table('txt_TRO_TNR/monica_v5.0_EM_CO2_TRO_ROA_LD0_2012_CO2_2012.txt', header = T, sep=";")
-lttrucks$mode<- "lttrucks"
-all_modes<- rbind(all_modes, lttrucks)
+lttrucks <- read.table(paste0(path, 'txt_TRO_TNR/monica_v5.0_EM_CO2_TRO_ROA_LD0_2012_CO2_2012.txt'), 
+                       header = T, sep = ";")
+lttrucks$mode <- "lttrucks"
+all_modes <- rbind(all_modes, lttrucks)
 
-mtw<- read.table('txt_TRO_TNR/monica_v5.0_EM_CO2_TRO_ROA_MC0_MP0_2012_CO2_2012.txt', header = T, sep=";")
-mtw$mode<- "mtw"
-all_modes<- rbind(all_modes, mtw)
+mtw <- read.table(paste0(path, 'txt_TRO_TNR/monica_v5.0_EM_CO2_TRO_ROA_MC0_MP0_2012_CO2_2012.txt'), 
+                  header = T, sep = ";")
+mtw$mode <- "mtw"
+all_modes <- rbind(all_modes, mtw)
 
-car<- read.table('txt_TRO_TNR/monica_v5.0_EM_CO2_TRO_ROA_PC0_2012_CO2_2012.txt', header = T, sep=";")
-car$mode<- "car"
-all_modes<- rbind(all_modes, car)
+car <- read.table(paste0(path, 'txt_TRO_TNR/monica_v5.0_EM_CO2_TRO_ROA_PC0_2012_CO2_2012.txt'), 
+                  header = T, sep = ";")
+car$mode <- "car"
+all_modes <- rbind(all_modes, car)
 
-other<- read.table('txt_TRO_TNR/monica_v5.0_EM_CO2_TNR_OTH_2012_CO2_2012.txt', header = T, sep=";")
-other$mode<- "other"
-all_modes<- rbind(all_modes, other)
+other <- read.table(paste0(path, 'txt_TRO_TNR/monica_v5.0_EM_CO2_TNR_OTH_2012_CO2_2012.txt'), 
+                    header = T, sep = ";")
+other$mode <- "other"
+all_modes <- rbind(all_modes, other)
 
-names(all_modes)[3]<-"co2emissions"
+names(all_modes)[3] <- "co2emissions"
 
 ##converting this data with geo-coordinates to a point shape file
-sp::coordinates(all_modes)<-~lon +lat
-proj4string(all_modes)<-CRS("+proj=longlat +datum=WGS84")
-all_modes<-SpatialPointsDataFrame(all_modes,as.data.frame(all_modes))
+sp::coordinates(all_modes) <- ~lon +lat
+proj4string(all_modes) <- CRS("+proj=longlat +datum=WGS84")
+all_modes <- SpatialPointsDataFrame(all_modes,as.data.frame(all_modes))
 ##could be saved as a shape file
 ##writeOGR(y,dsn='C:/Users/goelr/Work',layer='bus_co2_global3', driver="ESRI Shapefile")
 
 ####joining Buenos Aires and Cape Town######
 ## for these two cities, we do not already have the selection of coordinates within their city boundaries
-select<-rgdal::readOGR('buenos aires cape town.shp')
+#select<-rgdal::readOGR('buenos aires cape town.shp')
+select <- rgdal::readOGR('code/co2 emissions edgar/cities.shp')
 latlong = "+proj=longlat +datum=WGS84"
 select <- spTransform(select, CRS(latlong))
 
 ##intersecting emissions point shape file with city boundary polygon shape file of the two cities
-x<-(over(all_modes,select))
-x<- subset(x, select="Name")
-x<- cbind(x,all_modes )
-co2<- x[which(!is.na(x$Name)),]
+x <- (over(all_modes,select))
+x <- subset(x, select = "Name")
+x <- cbind(x,all_modes )
+co2 <- x[which(!is.na(x$Name)),]
 
-emissions<-co2 %>% group_by(Name, mode) %>% summarise("total"=sum(co2emissions))
-city_emissions<- spread(emissions,"mode","total" )
-city_emissions$total<- rowSums(city_emissions[,2:7])
-city_emissions[,2:7]<-round(city_emissions[,2:7]*100/city_emissions$total,2 )
+emissions <- co2 %>% group_by(Name, mode) %>% 
+  summarise("total" = sum(co2emissions))
+city_emissions <- spread(emissions, "mode", "total" )
+# --- Daniel
+# This is the object that I used for latam cities
+as.data.frame(city_emissions)
+#write.table(as.data.frame(city_emissions), "clipboard", sep = "\t", row.names = F)
+# --- Continue
+city_emissions$total <- rowSums(city_emissions[,2:7])
+city_emissions[,2:7] <- city_emissions[,2:7]/city_emissions$total
+city_emissions
 
-
-all_modes<- all_modes@data
+all_modes <- all_modes@data
 #all_modes$unique<- paste0(all_modes$lat, all_modes$lon)
 
 ###joining all other cities####
 ##reading the lookup table to join lat long with their respective cities (these geo-coordinates were identified by Nelson's team for PM2.5 emissions data)
-lookup<- read.csv('lat_long_ithim_cities_edgar_emissions_lookup.csv')
+lookup<- read.csv('code/co2 emissions edgar/lat_long_ithim_cities_edgar_emissions_lookup.csv')
 lookup<- lookup[which(lookup$File=="Total"),]
 lookup<- subset(lookup, select=c("lat", "lon","City"))
 
--- code/pm25 emissions edgar/pm25_transport_emissions_edgar_buenos aires cape town.R
+++ code/pm25 emissions edgar/pm25_transport_emissions_edgar_buenos aires cape town.R
@@ -9,51 +9,65 @@ library(tidyr)
 library(rgdal)
 library(sp)
 library(sf)
-setwd('code/pm25 emissions edgar/EDGARv4.3.2_PM2.5_TRO_TNR_data_2012')
-bus<- read.table('EDGAR_v4.3.2_PM25_EM_TRO_BS_PM25_2012.txt', header = T, sep=";")
-bus$mode<- "bus"
-all_modes<- bus
 
-htrucks<- read.table('EDGAR_v4.3.2_PM25_EM_TRO_HD_PM25_2012.txt', header = T, sep=";")
-htrucks$mode<- "htrucks"
-all_modes<- rbind(all_modes, htrucks)
+rm(list = ls())
+#setwd('code/pm25 emissions edgar/EDGARv4.3.2_PM2.5_TRO_TNR_data_2012')
+path <- 'code/pm25 emissions edgar/EDGARv4.3.2_PM2.5_TRO_TNR_data_2012/'
+bus <- read.table(paste0(path, 'EDGAR_v4.3.2_PM2.5_EM_TRO_BS_PM2.5_2012.txt'), 
+                 header = T, sep = ";")
+bus$mode <- "bus"
+all_modes <- bus
 
-lttrucks<- read.table('EDGAR_v4.3.2_PM25_EM_TRO_LD_PM25_2012.txt', header = T, sep=";")
-lttrucks$mode<- "lttrucks"
-all_modes<- rbind(all_modes, lttrucks)
+htrucks <- read.table(paste0(path, 'EDGAR_v4.3.2_PM2.5_EM_TRO_HD_PM2.5_2012.txt'),
+                      header = T, sep = ";")
+htrucks$mode <- "htrucks"
+all_modes <- rbind(all_modes, htrucks)
 
-mtw<- read.table('EDGAR_v4.3.2_PM25_TRO_MC_MP_PM25_2012.txt', header = T, sep=";")
-mtw$mode<- "mtw"
-all_modes<- rbind(all_modes, mtw)
+lttrucks <- read.table(paste0(path, 'EDGAR_v4.3.2_PM2.5_EM_TRO_LD_PM2.5_2012.txt'),
+                       header = T, sep = ";")
+lttrucks$mode <- "lttrucks"
+all_modes <- rbind(all_modes, lttrucks)
 
-car<- read.table('EDGAR_v4.3.2_PM25_EM_TRO_PC_PM25_2012.txt', header = T, sep=";")
-car$mode<- "car"
-all_modes<- rbind(all_modes, car)
+mtw <- read.table(paste0(path, 'EDGAR_v4.3.2_PM2.5_TRO_MC_MP_PM2.5_2012.txt'), 
+                  header = T, sep = ";")
+mtw$mode <- "mtw"
+all_modes <- rbind(all_modes, mtw)
 
-other<- read.table('EDGAR_v4.3.2_PM25_EM_TNR_OTH_RAI_PIP_2012.txt', header = T, sep=";")
-other$mode<- "other"
-all_modes<- rbind(all_modes, other)
+car <- read.table(paste0(path, 'EDGAR_v4.3.2_PM2.5_EM_TRO_PC_PM2.5_2012.txt'), 
+                  header = T, sep = ";")
+car$mode <- "car"
+all_modes <- rbind(all_modes, car)
 
-names(all_modes)[3]<-"pm25emissions"
+other <- read.table(paste0(path, 
+                           'EDGAR_v4.3.2_PM2.5_EM_TNR_OTH_RAI_PIP_2012.txt'), 
+                    header = T, sep = ";")
+other$mode <- "other"
+all_modes <- rbind(all_modes, other)
+
+names(all_modes)[3] <- "pm25emissions"
 
 ####joining Buenos Aires and Cape Town######
 ## for these two cities, we do not already have the selection of coordinates within their city boudnary
-sp::coordinates(all_modes)<-~lon +lat
-proj4string(all_modes)<-CRS("+proj=longlat +datum=WGS84")
-all_modes<-SpatialPointsDataFrame(all_modes,as.data.frame(all_modes))
+sp::coordinates(all_modes) <- ~lon +lat
+proj4string(all_modes) <- CRS("+proj=longlat +datum=WGS84")
+all_modes <- SpatialPointsDataFrame(all_modes,as.data.frame(all_modes))
 ##could be saved as a shape file
-##writeOGR(y,dsn='C:/Users/goelr/Work',layer='bus_co2_global3', driver="ESRI Shapefile")
+# writeOGR(all_modes,dsn='C:/Users/danie/Downloads',layer='bus_co2_global3', driver="ESRI Shapefile")
 
-select<-rgdal::readOGR('code/pm25 emissions edgar/buenos aires cape town.shp')
+#select <- rgdal::readOGR('code/pm25 emissions edgar/buenos aires cape town.shp')
+select <- rgdal::readOGR('code/pm25 emissions edgar/cities.shp')
 latlong = "+proj=longlat +datum=WGS84"
 select <- spTransform(select, CRS(latlong))
 ##intersection
-x<-(over(all_modes,select))
-x<- subset(x, select="Name")
-x<- cbind(x,all_modes )
-pm25<- x[which(!is.na(x$Name)),]
+x <- (over(all_modes,select))
+x <- subset(x, select = "Name")
+x <- cbind(x,all_modes)
+pm25 <- x[which(!is.na(x$Name)),]
 
-emissions<-pm25 %>% group_by(Name, mode) %>% summarise("total"=sum(pm25emissions))
-city_emissions<- spread(emissions,"mode","total" )
-city_emissions$total<- rowSums(city_emissions[,2:7])
-city_emissions[,2:7]<-round(city_emissions[,2:7]*100/city_emissions$total,2 )
+emissions <- pm25 %>% group_by(Name, mode) %>% 
+  summarise("total" = sum(pm25emissions))
+city_emissions <- spread(emissions, "mode", "total")
+city_emissions$total <- rowSums(city_emissions[,2:7], na.rm = T)
+city_emissions[,2:7] <- city_emissions[,2:7]/city_emissions$total
+as.data.frame(city_emissions)
+write.table(as.data.frame(city_emissions), "clipboard", sep = "\t", row.names = F)
