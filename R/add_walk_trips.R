7337af7595cf9e61fc52b9a95729ac8b29730c42
Ali Abbas <m.aliabbas@gmail.com>
2022-01-12 14:14:37 +0000

    Initialize all walk trips distance and duration as 0. Ref https://github.com/ITHIM/ITHIM-R/issues/84

--- R/add_walk_trips.R
+++ R/add_walk_trips.R
@@ -15,6 +15,9 @@ add_walk_trips <- function(pt_trips){
   #pt_trips <- arrange(pt_trips, trip_duration)
   walk_trips <- pt_trips
   walk_trips$stage_mode <- 'walk_to_pt'
+  
+  # Initialize with 0 duration
+  walk_trips$stage_duration <- walk_trips$stage_distance <- 0
   ##RJ all trips have the same BUS_WALK_TIME
   # walk_trips$stage_duration <- 0
   
@@ -38,27 +41,26 @@ add_walk_trips <- function(pt_trips){
       pt_trips$stage_duration[(pt_trips$stage_duration >= 25) & 
                                 (pt_trips$trip_mode == 'rail')] - RAIL_WALK_TIME
     
-  }else{
-    walk_trips$stage_duration <- 0
+    # Replace walk trips with duration greater than that of bus needs to be set to 0
+    if (any(walk_trips$stage_duration - pt_trips$stage_duration  >= 0))
+      walk_trips$stage_duration[(walk_trips$stage_duration - pt_trips$stage_duration)  >= 0] <- 0
+    
+    
+    # Correct walk trips distance
+    walk_trips$stage_distance <- (walk_trips$stage_duration / 60) * VEHICLE_INVENTORY$speed[VEHICLE_INVENTORY$stage_mode=='pedestrian']
+    pt_trips$stage_distance <- pt_trips$stage_distance - walk_trips$stage_distance
+    
+    # Recategorise trip_distance_cat for both bus and walk trips
+    pt_trips$trip_distance_cat[pt_trips$trip_distance > 0 & pt_trips$trip_distance < DIST_LOWER_BOUNDS[2]] <- DIST_CAT[1]
+    pt_trips$trip_distance_cat[pt_trips$trip_distance >= DIST_LOWER_BOUNDS[2] & pt_trips$trip_distance < DIST_LOWER_BOUNDS[3]] <- DIST_CAT[2]
+    pt_trips$trip_distance_cat[pt_trips$trip_distance >= DIST_LOWER_BOUNDS[3]] <- DIST_CAT[3]
+    
+    walk_trips$trip_distance_cat[walk_trips$trip_distance > 0 & walk_trips$trip_distance < DIST_LOWER_BOUNDS[2]] <- DIST_CAT[1]
+    walk_trips$trip_distance_cat[walk_trips$trip_distance >= DIST_LOWER_BOUNDS[2] & walk_trips$trip_distance < DIST_LOWER_BOUNDS[3]] <- DIST_CAT[2]
+    walk_trips$trip_distance_cat[walk_trips$trip_distance >= DIST_LOWER_BOUNDS[3]] <- DIST_CAT[3]
+    
   }
   
-  # Replace walk trips with duration greater than that of bus needs to be set to 0
-  if (any(walk_trips$stage_duration - pt_trips$stage_duration  >= 0))
-    walk_trips$stage_duration[(walk_trips$stage_duration - pt_trips$stage_duration)  >= 0] <- 0
-  
-  
-  # Correct walk trips distance
-  walk_trips$stage_distance <- (walk_trips$stage_duration / 60) * VEHICLE_INVENTORY$speed[VEHICLE_INVENTORY$stage_mode=='pedestrian']
-  pt_trips$stage_distance <- pt_trips$trip_distance - walk_trips$stage_distance
-  
-  # Recategorise trip_distance_cat for both bus and walk trips
-  pt_trips$trip_distance_cat[pt_trips$trip_distance > 0 & pt_trips$trip_distance < DIST_LOWER_BOUNDS[2]] <- DIST_CAT[1]
-  pt_trips$trip_distance_cat[pt_trips$trip_distance >= DIST_LOWER_BOUNDS[2] & pt_trips$trip_distance < DIST_LOWER_BOUNDS[3]] <- DIST_CAT[2]
-  pt_trips$trip_distance_cat[pt_trips$trip_distance >= DIST_LOWER_BOUNDS[3]] <- DIST_CAT[3]
-  
-  walk_trips$trip_distance_cat[walk_trips$trip_distance > 0 & walk_trips$trip_distance < DIST_LOWER_BOUNDS[2]] <- DIST_CAT[1]
-  walk_trips$trip_distance_cat[walk_trips$trip_distance >= DIST_LOWER_BOUNDS[2] & walk_trips$trip_distance < DIST_LOWER_BOUNDS[3]] <- DIST_CAT[2]
-  walk_trips$trip_distance_cat[walk_trips$trip_distance >= DIST_LOWER_BOUNDS[3]] <- DIST_CAT[3]
   return(list(pt_trips, walk_trips))
   
 }
