43ef35c26a663661c2098cfea526cfe34a7076a7
Ali Abbas <m.aliabbas@gmail.com>
2022-04-22 10:54:31 +0100

    Create trip pivot table for san antonio - for investigation

--- code/producing_trips_rd/trip_pivot_table_san_antonio.R
+++ code/producing_trips_rd/trip_pivot_table_san_antonio.R
@@ -4,7 +4,27 @@ library(tidyverse)
 
 io <- readRDS("results/multi_city/io.rds")
 
-tp <- io$san_antonio$trip_scen_sets %>% mutate(scenario = case_when(
+city <- 'buenos_aires'
+
+tp <- io[[city]]$trip_scen_sets %>% mutate(scenario = case_when(
+  scenario == "Scenario 1" ~ "CYC_SC",
+  scenario == "Scenario 2" ~ "CAR_SC",
+  scenario == "Scenario 3" ~ "BUS_SC",
+  scenario == "Baseline" ~ "Baseline")) 
+
+
+cities <- names(io)[!names(io) %in% 'scen_prop']
+
+co2_pc <- list()
+
+for (city in cities){
+  co2_pc[[city]] <- (colSums(io[[city]]$outcomes$co2_conc, na.rm = T) / sum(io[[city]]$demographic$population)) %>% 
+    as.data.frame() %>% tibble::rownames_to_column() %>% 
+    set_names("scenario", "val") %>% mutate(city = city)  %>% mutate(diff_base = 1 - (nth(val, 4) / val))
+  
+}
+
+co2_pc <- data.table::rbindlist(co2_pc) %>% mutate(scenario = case_when(
   scenario == "Scenario 1" ~ "CYC_SC",
   scenario == "Scenario 2" ~ "CAR_SC",
   scenario == "Scenario 3" ~ "BUS_SC",
