f3c684fa6c78b2659fa62c7a7cf816de78510cc5
Anna Schroeder <anna.schroeder@outlook.de>
2022-05-26 09:42:11 +0100

    Updated input parameters: injury_reporting_rate and truck_to_car_ratio

--- multi_city_script.R
+++ multi_city_script.R
@@ -18,7 +18,7 @@ cities <- c('antofagasta', 'arica', 'belo_horizonte', 'bogota', 'buenos_aires',
 
 # number of times input values are sampled from each input parameter distribution
 nsamples <- 1
-input_parameter_file <- "InputParameters_v3.0.xlsx"
+input_parameter_file <- "InputParameters_v4.0.xlsx"
 output_version <- "v0.3" # gives the version number of the output documents, independent of the input parameter file name
 author <- "AA"
 comment <- "Added CO2 emission sampling"
