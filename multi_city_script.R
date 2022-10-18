46fae01bf3bf559036f4ce3beb1e24a962ae73cd
usr110 <m.aliabbas@gmail.com>
2022-10-18 20:10:07 +0100

    Set it to run for sp city - and remove unused code

--- multi_city_script.R
+++ multi_city_script.R
@@ -32,7 +32,7 @@ cities <- c('antofagasta', 'arica', 'belo_horizonte', 'bogota', 'buenos_aires',
 
 cities <- 'sao_paulo'
 
-input_parameter_file <- "InputParameters_v15.0.xlsx"
+input_parameter_file <- "InputParameters_v16.0.xlsx"
 
 output_version <- "v0.3" # gives the version number of the output documents, independent of the input parameter file name
 author <- "AA"
@@ -232,47 +232,6 @@ print(system.time(for(city in cities){
   ithim_objects[[city]]$injury_table <- INJURY_TABLE
   ithim_objects[[city]]$vehicle_inventory <- VEHICLE_INVENTORY
   
-  ## store results to plot
-  # min_ages <- sapply(ithim_objects[[city]]$outcome$hb$ylls$age_cat,function(x)as.numeric(strsplit(x,'-')[[1]][1]))
-  # max_ages <- sapply(ithim_objects[[city]]$outcome$hb$ylls$age_cat,function(x)as.numeric(strsplit(x,'-')[[1]][2]))
-  # sub_outcome <- subset(ithim_objects[[city]]$outcome$hb$ylls,
-  #                       min_ages >= min_age & max_ages <= max_age)
-  # result_mat <- colSums(sub_outcome[,3:ncol(sub_outcome)])
-  # columns <- length(result_mat)
-  # nDiseases <- columns/NSCEN
-  # if (city == cities[1]) {
-  #   disease_list <- list()
-  #   for (i in 1:nDiseases) disease_list[[i]] <- toplot
-  # }
-  # min_pop_ages <- sapply(DEMOGRAPHIC$age,function(x)as.numeric(strsplit(x,'-')[[1]][1]))
-  # max_pop_ages <- sapply(DEMOGRAPHIC$age,function(x)as.numeric(strsplit(x,'-')[[1]][2]))
-  # for (i in 1:nDiseases)
-  #   disease_list[[i]][,which(cities == city)] <- result_mat[1:NSCEN + (i - 1) * NSCEN]/sum(subset(DEMOGRAPHIC,min_pop_ages >= min_age & max_pop_ages <= max_age)$population)
 }))
-
-
-# {x11(width = 10, height = 5);
-#   layout.matrix <- matrix(c(2:6,1,7:12), nrow = 2, ncol = 6,byrow = T)
-#   graphics::layout(mat = layout.matrix, heights = c(2,3), 
-#                    widths = c(2.8,2,2,2,2,2.5))
-#   ylim <- range(result_mat)
-#   cols <- rainbow(length(cities))
-#   mar1 <- rep(7, nDiseases); mar1[1:6] <- 1
-#   mar2 <- rep(1, nDiseases); mar2[c(2,7)] <- 6; mar2[c(1,12)] <- 3
-#   for (i in 1:nDiseases) {
-#     ylim <- if (i %in% c(1,12)) range(disease_list[[i]]) else c(-11,4)*1e-4
-#     par(mar = c(mar1[i], mar2[i], 4, 1))
-#     barplot(t(disease_list[[i]]), ylim = ylim, las = 2, beside = T, 
-#             col = cols, 
-#             names.arg = if (i < 7) NULL else rownames(SCENARIO_PROPORTIONS), 
-#             main = paste0(last(strsplit(names(result_mat)[i * NSCEN], '_')[[1]])),
-#             yaxt='n')
-#     if (i %in% c(2,1,7,12)) {
-#       axis(2,cex.axis=1.5); if(i%in%c(2,7)) mtext(side=2,'YLL gain per person',line=3)}
-#     if (i == nDiseases - 1) legend(legend = cities, fill = cols, bty = 'n',
-#                                    y = -1e-5, x = 5, cex = 0.9)
-#   }
-# }
-
 
 saveRDS(ithim_objects, "results/multi_city/io.rds", version = 2)
