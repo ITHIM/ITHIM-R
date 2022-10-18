2e1faabdb6ab2de00744dce9e3c8beed707f2435
usr110 <m.aliabbas@gmail.com>
2022-10-18 20:15:05 +0100

    Adjust path

--- master_script_constant_mode.R
+++ master_script_constant_mode.R
@@ -7,10 +7,10 @@ source("multi_city_script.R")
 
 # Once the multi_city_script.R has run, the ithim_object (io) is saved at results/multi_city/io.rds
 # Knit summary document for dist/dur and scenario settings
-rmarkdown::render("summary_tables.Rmd")
+rmarkdown::render("rmd_docs/summary_tables.Rmd")
 
 # Knit summary document for PA and AP distribution
-rmarkdown::render("summary_tables_PA_AP.Rmd")
+rmarkdown::render("rmd_docs/summary_tables_PA_AP.Rmd")
 
 # Knit summary document for dist/dur and scenario settings
-rmarkdown::render("injury_tables.Rmd")
+rmarkdown::render("rmd_docs/injury_tables.Rmd")
