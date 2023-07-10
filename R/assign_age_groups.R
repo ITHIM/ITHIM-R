d1845d2e27cf232ff38f467eb0f9b16a8cf87fa0
Anna Schroeder <aks85@mrc-epid.cam.ac.uk>
2023-07-10 08:39:02 +0100

    added documentation for assign_age_groups.R

--- R/assign_age_groups.R
+++ R/assign_age_groups.R
@@ -1,8 +1,9 @@
 #' Assign age groups to individuals
 #' 
-#' Prunes dataset given max and min ages; assigns age group labels given age
+#' Prunes dataset given max and min ages, i.e. removes entries of people with ages outside the scope of the model.
+#' Assigns age group labels given age
 #' 
-#' @param dataset data frame to be edited
+#' @param dataset data frame to which age categories are to be added and ages outside model scope are removed
 #' @param age_category vector of strings giving age categories
 #' @param age_lower_bounds lower boundaries of age categories
 #' @param max_age maximum age for model
@@ -12,13 +13,19 @@
 #' @return edited data frame
 #' 
 #' @export
-assign_age_groups <- function(dataset,age_category=AGE_CATEGORY,age_lower_bounds=AGE_LOWER_BOUNDS,max_age=MAX_AGE,min_age=AGE_LOWER_BOUNDS[1],age_label='age'){
+assign_age_groups <- function(dataset,age_category=AGE_CATEGORY,age_lower_bounds=AGE_LOWER_BOUNDS,
+                              max_age=MAX_AGE,min_age=AGE_LOWER_BOUNDS[1],age_label='age'){
+  
+  # remove any entries with ages above or below the age range considered in the model. Remove any entries with missing age information
   dataset <- dataset[dataset[[age_label]]<=max_age&!is.na(dataset[[age_label]])&dataset[[age_label]]>=min_age,]
+  # initialise the age category
   dataset$age_cat <- 0
   ##!! assuming more than one age category
+  # add age categories
   for(i in 2:length(age_lower_bounds)-1){
     dataset$age_cat[dataset[[age_label]] >= age_lower_bounds[i] & dataset[[age_label]] < age_lower_bounds[i+1]] <- age_category[i]
   }
+  # add age catetory for highest age group
   dataset$age_cat[dataset[[age_label]] >= age_lower_bounds[length(age_lower_bounds)]] <- age_category[length(age_lower_bounds)]
   dataset
 }
