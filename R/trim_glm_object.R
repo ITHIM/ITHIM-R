04534875fee45d4ab04d44052cee71cfd6435cdc
Anna Schroeder <anna.schroeder@outlook.de>
2023-09-18 14:17:32 +0200

    added remaining deleted functions

---
+++ R/trim_glm_object.R
@@ -0,0 +1,30 @@
+#' Reduce size of glm object - CURRENTLY NOT BEING CALLED
+#' 
+#' Delete some attributes of glm object in order to save space
+#' 
+#' @param obj glm object
+#' 
+#' @return glm object
+#' 
+#' @export
+trim_glm_object <- function(obj){
+  obj$y <- c()
+  obj$model <- c()
+  obj$R <- c()
+  obj$qr$qr <- c()
+  obj$residuals <- c()
+  obj$fitted.values <- c()
+  obj$effects <- c()
+  obj$linear.predictors <- c()
+  obj$weights <- c()
+  obj$prior.weights <- c()
+  obj$data <- c()
+  obj$family$variance = c()
+  obj$family$dev.resids = c()
+  obj$family$aic = c()
+  obj$family$validmu = c()
+  obj$family$simulate = c()
+  #attr(obj$terms,".Environment") = c()
+  attr(obj$formula,".Environment") = c()
+  obj
+}
