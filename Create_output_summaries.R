557fce19f580c5fa221e5d77a6c08da9f1d29691
Ali Abbas <m.aliabbas@gmail.com>
2025-03-05 13:18:24 +0000

    Comment out producing injury related markdown doc

--- Create_output_summaries.R
+++ Create_output_summaries.R
@@ -30,7 +30,7 @@ rmarkdown::render('summary_tables_PA_AP.Rmd', params = list(output_version = out
 rmarkdown::render('summary_tables.Rmd', params = list(output_version = output_version))
 
 # create injury summary tables
-rmarkdown::render('injury_tables.Rmd', params = list(output_version = output_version))
+# rmarkdown::render('injury_tables.Rmd', params = list(output_version = output_version))
 
 
 
