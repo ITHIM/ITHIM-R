77b029474704235719a51e98f4ce7d589acc8424
Ali Abbas <m.aliabbas@gmail.com>
2025-05-08 15:02:29 +0100

    Add code to generate health outcomes

--- multi_city_script.R
+++ multi_city_script.R
@@ -54,6 +54,7 @@ library(janitor)
 
 # Disable scientific notation
 options(scipen = 999)
+options(datatable.fread.datatable=FALSE)
 
 if (!require("drpa",character.only = TRUE)) {
   print('Installing "drpa" package...')
@@ -238,7 +239,7 @@ ithim_objects <- outcome <- outcome_pp <- yll_per_hundred_thousand <- list()
     PM_emission_inventory = PM_emission_inventories[[city]],
     CO2_emission_inventory = CO2_emission_inventories[[city]],
     speeds = speeds[[city]],
-    
+    ADD_WALK_TO_PT_TRIPS = T,
     CYCLING_MMET =	cycling_mmet,
     WALKING_MMET =	walking_mmet,
     PASSENGER_MMET =	passenger_mmet,
@@ -252,8 +253,6 @@ ithim_objects <- outcome <- outcome_pp <- yll_per_hundred_thousand <- list()
     AP_DOSE_RESPONSE_QUANTILE = ap_dr_quantile,
     PM_CONC_BASE = pm_conc_base[[city]],  
     PM_TRANS_SHARE = pm_trans_share[[city]],  
-    
-    
     ADD_BUS_DRIVERS = F,
     ADD_CAR_DRIVERS = F,
     SCENARIO_NAME = scenario_name,
@@ -319,9 +318,131 @@ ithim_objects$ithim_run$output_version <- output_version
 ithim_objects$ithim_run$author <- author
 ithim_objects$ithim_run$comment <- comment
 
+get_qualified_scen_name <- function(cs){
+  qualified_scen_name <- ""
+  if (cs == 'base' | cs == 'baseline' | cs == 'Baseline')
+    qualified_scen_name <- 'Baseline'
+  else if(cs == "sc_walk")
+    qualified_scen_name <- 'Walking'
+  else if(cs == "sc_cycle")
+    qualified_scen_name <- 'Cycling'
+  else if(cs == "sc_car")
+    qualified_scen_name <- 'Car'
+  else if(cs == "sc_motorcycle")
+    qualified_scen_name <- 'Motorcycling'
+  else if(cs == "sc_bus")
+    qualified_scen_name <- 'Bus'
+  
+  return(qualified_scen_name)
+}
+
+# initialise variables
+health <- list()
+measure <- c("ylls", "deaths")
+pathway <- c("hb", "pathway_hb")
+
+for (k in measure) { # loop for ylls or deaths
+  for (j in pathway) { # loop for hb or pathway_hb
+    for (i in names(ithim_objects)[!names(ithim_objects) %in% c('scen_prop','ithim_run' )]) { # loop for cities
+      
+      # Temporal dataset for health outcomes
+      temp <- ithim_objects[[i]]$outcomes[[j]][[k]]
+      
+      # Total population
+      #overall_pop <- sum(io[[i]]$demographic$population)
+      
+      # Population by age and sex
+      pop_by_age_sex <- ithim_objects[[i]]$demographic %>% group_by(age, sex) %>%
+        summarize(pop_age_sex = sum(population))
+      
+      # get the health burden into the correct format by creating a long format and adding additional columns
+      # create long format
+      health[[k]][[j]][[i]] <- gather(temp, key = "cause", "measure",
+                                      -sex, -age_cat) %>%
+        mutate(city = i, # create city column
+               scenario = case_when( # create scenario column
+                 grepl("sc_cycle", cause) ~ get_qualified_scen_name('sc_cycle'),
+                 grepl("sc_car", cause) ~ get_qualified_scen_name('sc_car'),
+                 grepl("sc_bus", cause) ~ get_qualified_scen_name('sc_bus'),
+                 grepl("sc_motorcycle", cause) ~ get_qualified_scen_name('sc_motorcycle')
+               ),
+               level1 = case_when( # create level 1 column
+                 grepl("all_cause", cause) ~ "L1: All Cause",
+                 grepl("inj", cause) ~ "L1: RTI"
+               ),
+               level2 = case_when( # create level 2 column
+                 grepl("total_cancer", cause) ~ "L2: Cancer",
+                 grepl("CVD", cause) ~ "L2: CVD",
+                 grepl("respiratory", cause) ~ "L2: Respiratory",
+                 grepl("T2D", cause) ~ "L2: Other",
+                 grepl("total_dementia", cause) ~ "L2: Other",
+                 grepl("Parkinson", cause) ~ "L2: Other",
+                 grepl("inj", cause) ~ "L2: RTI"
+               ),
+               level3 = case_when( # create level 3 column
+                 grepl("IHD", cause) ~ "L3: IHD",
+                 grepl("lung_cancer", cause) ~ "L3: lung_cancer",
+                 grepl("COPD", cause) ~ "L3: COPD",
+                 grepl("stroke", cause) ~ "L3: stroke",
+                 grepl("T2D", cause) ~ "L3: T2D",
+                 grepl("LRI", cause) ~ "L3: LRI",
+                 grepl("breast_cancer", cause) ~ "L3: breast_cancer",
+                 grepl("colon_cancer", cause) ~ "L3: colon_cancer",
+                 grepl("endo_cancer", cause) ~ "L3: endo_cancer",
+                 grepl("liver_cancer", cause) ~ "L3: liver_cancer",
+                 grepl("total_dementia", cause) ~ "L3: total_dementia",
+                 grepl("myeloma", cause) ~ "L3: myeloma",
+                 grepl("myeloid_leukemia", cause) ~ "L3: Myeloid Leukemia",
+                 grepl("Parkinson", cause) ~ "L3: Parkinson",
+                 grepl("head_neck_cancer", cause) ~ "L3: head_neck_cancer",
+                 grepl("stomach_cancer", cause) ~ "L3: stomach_cancer",
+                 grepl("inj", cause) ~ "L3: RTI"
+               ),
+               dose = case_when( # create dose column
+                 grepl("_pa_ap_", cause) ~ "PA and AP",
+                 grepl("_pa_", cause) ~ "PA",
+                 grepl("_ap_", cause) ~ "AP",
+                 grepl("_inj", cause) ~ "RTI"
+               )
+        ) %>% # End mutate
+        # join with demographic information
+        left_join(pop_by_age_sex, by = c("age_cat" = "age", "sex" = "sex"))
+      
+      # %>%
+      # create new columns showing burden per 100000 people by age or by age and sex
+      # mutate(metric_100k = measure / overall_pop * 100000,
+      #        metric_100k_sex = measure / overall_pop * 100000)
+      
+    } # Loop for each city
+  } # Loop for each pathway
+} # Loop for each measure
+
+
+# for each health burden and pathway join all cities into one dataframe
+ylls <-  rbindlist(health$ylls$hb, use.names = T) #|> filter(!str_detect(cause, "_lb") & !str_detect(cause, "_ub"))
+ylls_pathway <- rbindlist(health$ylls$pathway_hb, use.names = T) #|> filter(!str_detect(cause, "_lb") & !str_detect(cause, "_ub"))
+deaths <- rbindlist(health$deaths$hb, use.names = T) #|> filter(!str_detect(cause, "_lb") & !str_detect(cause, "_ub"))
+deaths_pathway <- rbindlist(health$deaths$pathway_hb, use.names = T) #|> filter(!str_detect(cause, "_lb") & !str_detect(cause, "_ub"))
+
+# write csv without output version number
+write.csv(ylls,
+          paste0('results/multi_city/health_impacts/ylls.csv'),
+          row.names = F)
+# Export ylls PATHWAY_HB
+write.csv(ylls_pathway,
+          paste0('results/multi_city/health_impacts/ylls_pathway.csv'),
+          row.names = F)
+# Export deaths HB
+write.csv(deaths,
+          paste0('results/multi_city/health_impacts/deaths.csv'),
+          row.names = F)
+# Export deaths PATHWAY_HB
+write.csv(deaths_pathway,
+          paste0('results/multi_city/health_impacts/deaths_pathway.csv'),
+          row.names = F)
+
+
 saveRDS(ithim_objects, paste0("results/multi_city/io_",output_version,".rds"), version = 2)
-
-
 
 # add to output control document
 
