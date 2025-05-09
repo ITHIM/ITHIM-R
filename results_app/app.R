47d53ad71a1802f17489da5bcd41e8ca55fa60d1
Ali Abbas <m.aliabbas@gmail.com>
2025-05-09 15:33:44 +0100

    Use generic io

--- results_app/app.R
+++ results_app/app.R
@@ -33,7 +33,7 @@ output_version <- repo_sha
 
 # Assumes that multi_city_script.R has been run  
 # read in input file
-io <- readRDS(paste0("../results/multi_city/io_", output_version, ".rds"))
+io <- readRDS(paste0("../results/multi_city/io.rds"))
 
 # io <- readRDS(paste0("../results/multi_city/io_48d87be.rds"))
 # io <- readRDS(paste0("../results/multi_city/io_c492320.rds"))
